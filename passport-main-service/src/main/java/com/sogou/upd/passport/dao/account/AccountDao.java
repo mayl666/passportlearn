26dd27663ac4bffcb8a875dcebf44c3e90adb547
liuling <liuling@sogou-inc.com>
2013-03-22 16:51:26 +0800

    用户主表的dao接口和实现

---
+++ passport-main-service/src/main/java/com/sogou/upd/passport/dao/account/AccountDao.java
@@ -0,0 +1,25 @@
+package com.sogou.upd.passport.dao.account;
+
+import com.sogou.upd.passport.model.account.Account;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liuling
+ * Date: 13-3-22
+ * Time: 下午4:35
+ * To change this template use File | Settings | File Templates.
+ */
+public interface AccountDao {
+    /**
+     * 查询某手机用户是否已经注册
+     * @param account 封装的对象
+     * @return
+     */
+    public Account findUserRegisterIsOrNot(Account account);
+
+    /**
+     * 用户注册
+     * @param account  封装的对象
+     */
+    public void userRegister(Account account);
+}
