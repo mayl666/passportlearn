ab2fff3d273d6ac21d74c19976cbed5fa4def435
liuling <liuling@sogou-inc.com>
2013-03-22 16:26:30 +0800

    测试

--- passport-main-service/src/main/java/com/sogou/upd/passport/dao/account/AccountMapper.java
+++ passport-main-service/src/main/java/com/sogou/upd/passport/dao/account/AccountMapper.java
@@ -1,5 +1,7 @@
 package com.sogou.upd.passport.dao.account;
 
+import com.sogou.upd.passport.model.account.Account;
+
 /**
  * User: mayan
  * Date: 13-3-22
@@ -7,4 +9,17 @@ package com.sogou.upd.passport.dao.account;
  * To change this template use File | Settings | File Templates.
  */
 public interface AccountMapper {
+    /**
+     * 根据传入的参数，手机号码和密码,查询该手机是否已经注册
+     * @return
+     */
+    public Account findUserRegisterIsOrNot(Account account);
+
+    /**
+     * 验证合法，用户注册
+     * @param account
+     */
+    public void userRegister(Account account);
+
+
 }
