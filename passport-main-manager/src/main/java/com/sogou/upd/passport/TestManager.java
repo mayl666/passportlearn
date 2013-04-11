c2c3cd67d39a6200ce9143682dd1c1f21598ee42
mayan <mayan@sogou-inc.com>
2013-04-11 16:50:01 +0800

    添加dubbo,删除没用文件

---
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/TestManager.java
@@ -0,0 +1,11 @@
+package com.sogou.upd.passport;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mayan
+ * Date: 13-4-11
+ * Time: 下午4:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestManager {
+}
