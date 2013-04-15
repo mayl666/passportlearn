7e3e9a80cc1e56cf95b4951b31304435ce7d19f3
mayan <mayan@sogou-inc.com>
2013-04-15 16:35:12 +0800

    添加新的manager管理

---
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/account/AccountLoginManager.java
@@ -0,0 +1,11 @@
+package com.sogou.upd.passport.manager.account;
+
+/**
+ * 手机号登录，邮箱登录
+ * User: mayan
+ * Date: 13-4-15
+ * Time: 下午4:33
+ * To change this template use File | Settings | File Templates.
+ */
+public interface AccountLoginManager {
+}
---
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/account/MobileAccountLoginManager.java
@@ -0,0 +1,11 @@
+package com.sogou.upd.passport.manager.account;
+
+/**
+ * 手机用户登录管理
+ * User: mayan
+ * Date: 13-4-15
+ * Time: 下午4:33
+ * To change this template use File | Settings | File Templates.
+ */
+public interface MobileAccountLoginManager {
+}
