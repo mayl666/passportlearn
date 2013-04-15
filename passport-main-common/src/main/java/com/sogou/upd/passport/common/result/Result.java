a5836f56a3433325f56f5a6258693750562e4350
mayan <mayan@sogou-inc.com>
2013-04-15 19:21:43 +0800

    添加新的manager管理

--- passport-main-common/src/main/java/com/sogou/upd/passport/common/result/Result.java
+++ passport-main-common/src/main/java/com/sogou/upd/passport/common/result/Result.java
@@ -1,6 +1,11 @@
 package com.sogou.upd.passport.common.result;
 
-import java.util.*;
+import net.sf.json.JSONObject;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Service返回值对象
@@ -10,47 +15,84 @@ import java.util.*;
  * To change this template use File | Settings | File Templates.
  */
 public class Result {
+
     private boolean success;
-    private Object obj;
-    private List list;
-    private String message;
+
+    /**
+     * service返回的对象
+     */
+    private Map<String,Object> data = new HashMap<String,Object>();
+
+    private String status;
+    private String statusText;
 
     public Result() {
     }
 
-    public Result(String message) {
-        this.message = message;
+    /**
+     * 新增一个带key的返回结果
+     * @param key
+     * @param obj
+     * @return
+     */
+    public Object addDefaultModel(String key,Object obj) {
+        return data.put(key,obj);
+    }
+    /**
+     * 取出整个map对象
+     * @return
+     */
+    public Map<String,Object> getData() {
+        return data;
+    }
+    /**
+     * 返回是否成功
+     * @return
+     */
+    public boolean getSuccess() {
+        return success;
     }
 
     public boolean isSuccess() {
         return success;
     }
 
+    /**
+     * 设置返回是否成功
+     * @param success
+     */
     public void setSuccess(boolean success) {
         this.success = success;
     }
 
-    public Object getObj() {
-        return obj;
+    public String getStatus() {
+        return status;
     }
 
-    public void setObj(Object obj) {
-        this.obj = obj;
+    public void setStatus(String status) {
+        this.status = status;
     }
 
-    public List getList() {
-        return list;
+    public String getStatusText() {
+        return statusText;
     }
 
-    public void setList(List list) {
-        this.list = list;
+    public void setStatusText(String statusText) {
+        this.statusText = statusText;
     }
 
-    public String getMessage() {
-        return message;
+    public static void main(String[] args) {
+        Result result1=new Result();
+        result1.setSuccess(true);
+
+        result1.addDefaultModel("smscode","65652") ;
+        result1.setStatus("1000");
+        result1.setStatusText("获取注册验证码成功");
+
+        JSONObject jsonObject= JSONObject.fromObject(result1);
+
+
+        System.out.println(jsonObject.toString());
     }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
