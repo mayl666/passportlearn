0ebdf61a37582852c3ccd18957cb16862bbf39fc
shipengzhi <shipengzhi@sogou-inc.com>
2013-05-09 22:16:57 +0800

    修改client_id为空时，抛出服务器异常的bug

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/account/action/AccountAction.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/action/AccountAction.java
@@ -1,41 +1,23 @@
 package com.sogou.upd.passport.web.account.action;
 
 import com.google.common.base.Strings;
-
 import com.sogou.upd.passport.common.result.Result;
 import com.sogou.upd.passport.common.utils.ErrorUtil;
-import com.sogou.upd.passport.common.utils.StringUtil;
 import com.sogou.upd.passport.manager.account.AccountManager;
 import com.sogou.upd.passport.manager.account.AccountRegManager;
 import com.sogou.upd.passport.manager.app.ConfigureManager;
 import com.sogou.upd.passport.manager.form.ActiveEmailParameters;
-import com.sogou.upd.passport.manager.form.MoblieCodeParams;
 import com.sogou.upd.passport.manager.form.WebRegisterParameters;
 import com.sogou.upd.passport.web.BaseController;
 import com.sogou.upd.passport.web.ControllerHelper;
-
-import org.apache.velocity.Template;
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.Velocity;
-import org.apache.velocity.app.VelocityEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.awt.image.BufferedImage;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-
-import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -47,92 +29,103 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/web")
 public class AccountAction extends BaseController {
 
-  private static final Logger logger = LoggerFactory.getLogger(AccountAction.class);
+    private static final Logger logger = LoggerFactory.getLogger(AccountAction.class);
 
-  @Autowired
-  private AccountRegManager accountRegManager;
-  @Autowired
-  private AccountManager accountManager;
-  @Autowired
-  private ConfigureManager configureManager;
+    @Autowired
+    private AccountRegManager accountRegManager;
+    @Autowired
+    private AccountManager accountManager;
+    @Autowired
+    private ConfigureManager configureManager;
 
-  /*
-     web注册页跳转
-   */
-  @RequestMapping(value = "/register", method = RequestMethod.GET)
-  public String register(HttpServletRequest request, HttpServletResponse response)
-      throws Exception {
+    /*
+       web注册页跳转
+     */
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String register(HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
 
-     return "reg";
-  }
-
-  /**
-   * web页面注册
-   *
-   * @param regParams 传入的参数
-   */
-  @RequestMapping(value = "/reguser", method = RequestMethod.POST)
-  @ResponseBody
-  public Object reguser(HttpServletRequest request, WebRegisterParameters regParams)
-      throws Exception {
-    //参数验证
-    String validateResult = ControllerHelper.validateParams(regParams);
-    if (!Strings.isNullOrEmpty(validateResult)) {
-      return Result.buildError(ErrorUtil.ERR_CODE_COM_REQURIE, validateResult);
-    }
-    //验证client_id
-    String client_id=regParams.getClient_id() ;
-    if(!StringUtil.checkIsDigit(client_id)){
-      return Result.buildError(ErrorUtil.ERR_CODE_COM_REQURIE);
+        return "reg";
     }
 
-    int clientId = Integer.parseInt(client_id);
-    String username = regParams.getUsername();
-    String password = regParams.getPassword();
-    String code = regParams.getCode();
+    /**
+     * web页面注册
+     *
+     * @param regParams 传入的参数
+     */
+    @RequestMapping(value = "/reguser", method = RequestMethod.POST)
+    @ResponseBody
+    public Object reguser(HttpServletRequest request, WebRegisterParameters regParams)
+            throws Exception {
+        //参数验证
+        String validateResult = ControllerHelper.validateParams(regParams);
+        if (!Strings.isNullOrEmpty(validateResult)) {
+            return Result.buildError(ErrorUtil.ERR_CODE_COM_REQURIE, validateResult);
+        }
+        //验证client_id
+        int clientId;
+        try {
+            clientId = Integer.parseInt(regParams.getClient_id());
+        } catch (NumberFormatException e) {
+            return Result.buildError(ErrorUtil.ERR_FORMAT_CLIENTID);
+        }
+        //检查client_id是否存在
+        if (!configureManager.checkAppIsExist(clientId)) {
+            return Result.buildError(ErrorUtil.INVALID_CLIENTID);
+        }
 
-    //todo 验证码校验
+        String username = regParams.getUsername();
+        String password = regParams.getPassword();
+        String code = regParams.getCode();
 
-    //检查client_id格式以及client_id是否存在
-    if (!configureManager.checkAppIsExist(clientId)) {
-      return Result.buildError(ErrorUtil.INVALID_CLIENTID);
+        //todo 验证码校验
+
+        //检查client_id格式以及client_id是否存在
+        if (!configureManager.checkAppIsExist(clientId)) {
+            return Result.buildError(ErrorUtil.INVALID_CLIENTID);
+        }
+
+        Result result = null;
+        //验证用户是否注册过
+        if (!accountManager.isAccountExists(username)) {
+            String ip = getIp(request);
+            result = accountRegManager.webRegister(regParams, ip);
+        } else {
+            result = result.buildError(ErrorUtil.ERR_CODE_ACCOUNT_REGED);
+        }
+        return result;
     }
 
-    Result result = null;
-    //验证用户是否注册过
-    if (!accountManager.isAccountExists(username)) {
-      String ip = getIp(request);
-      result=accountRegManager.webRegister(regParams,ip);
-    } else {
-      result=result.buildError(ErrorUtil.ERR_CODE_ACCOUNT_REGED);
+    /**
+     * 邮件激活
+     *
+     * @param activeParams 传入的参数
+     */
+    @RequestMapping(value = "/activemail", method = RequestMethod.GET)
+    @ResponseBody
+    public Object activeEmail(HttpServletRequest request, ActiveEmailParameters activeParams)
+            throws Exception {
+
+        //参数验证
+        String validateResult = ControllerHelper.validateParams(activeParams);
+        if (!Strings.isNullOrEmpty(validateResult)) {
+            return Result.buildError(ErrorUtil.ERR_CODE_COM_REQURIE, validateResult);
+        }
+        //验证client_id
+        int clientId;
+        try {
+            clientId = Integer.parseInt(activeParams.getClient_id());
+        } catch (NumberFormatException e) {
+            return Result.buildError(ErrorUtil.ERR_FORMAT_CLIENTID);
+        }
+        //检查client_id是否存在
+        if (!configureManager.checkAppIsExist(clientId)) {
+            return Result.buildError(ErrorUtil.INVALID_CLIENTID);
+        }
+
+        //邮件激活
+        Result result = accountRegManager.activeEmail(activeParams);
+
+        return result;
     }
-    return result;
-  }
-
-  /**
-   * 邮件激活
-   *
-   * @param activeParams 传入的参数
-   */
-  @RequestMapping(value = "/activemail", method = RequestMethod.GET)
-  @ResponseBody
-  public Object activeEmail(HttpServletRequest request, ActiveEmailParameters activeParams)
-      throws Exception {
-
-    //参数验证
-    String validateResult = ControllerHelper.validateParams(activeParams);
-    if (!Strings.isNullOrEmpty(validateResult)) {
-      return Result.buildError(ErrorUtil.ERR_CODE_COM_REQURIE, validateResult);
-    }
-    //验证client_id
-    String client_id=activeParams.getClient_id() ;
-    if(!StringUtil.checkIsDigit(client_id)){
-      return Result.buildError(ErrorUtil.ERR_CODE_COM_REQURIE);
-    }
-
-    //邮件激活
-    Result result = accountRegManager.activeEmail(activeParams);
-
-    return result;
-  }
 }
