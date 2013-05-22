2ccb5602370b4f7a88dd609518820a2892631b88
hujunfei <hujunfei@sogou-inc.com>
2013-05-22 14:55:25 +0800

    产生secureCode识别上一步成功状态

---
+++ passport-main-service/src/main/java/com/sogou/upd/passport/service/account/AccountSecureService.java
@@ -0,0 +1,54 @@
+package com.sogou.upd.passport.service.account;
+
+import com.sogou.upd.passport.exception.ServiceException;
+
+/**
+ * Created with IntelliJ IDEA. User: hujunfei Date: 13-5-21 Time: 上午10:51 To change this template
+ * use File | Settings | File Templates.
+ */
+public interface AccountSecureService {
+
+    /**
+     * 产生secureCode，放入缓存，设置有效时间；返回secureCode——重置密码
+     *
+     * @param passportId
+     * @param clientId
+     * @return
+     * @throws ServiceException
+     */
+    public String getSecureCodeResetPwd(String passportId, int clientId) throws ServiceException;
+
+    /**
+     * 检测secureCode——重置密码
+     *
+     * @param passportId
+     * @param clientId
+     * @param secureCode
+     * @return
+     * @throws ServiceException
+     */
+    public boolean checkSecureCodeResetPwd(String passportId, int clientId, String secureCode)
+            throws ServiceException;
+
+    /**
+     * 产生secureCode，放入缓存，设置有效时间；返回secureCode——修改密保内容
+     *
+     * @param passportId
+     * @param clientId
+     * @return
+     * @throws ServiceException
+     */
+    public String getSecureCodeModSecureInfo(String passportId, int clientId) throws ServiceException;
+
+    /**
+     * 检测secureCode——记录前一步操作成功
+     *
+     * @param passportId
+     * @param clientId
+     * @param secureCode
+     * @return
+     * @throws ServiceException
+     */
+    public boolean checkSecureCodeModSecureInfo(String passportId, int clientId, String secureCode)
+            throws ServiceException;
+}
---
+++ passport-main-service/src/main/java/com/sogou/upd/passport/service/account/impl/AccountSecureServiceImpl.java
@@ -0,0 +1,81 @@
+package com.sogou.upd.passport.service.account.impl;
+
+import com.sogou.upd.passport.common.CacheConstant;
+import com.sogou.upd.passport.common.DateAndNumTimesConstant;
+import com.sogou.upd.passport.common.utils.RedisUtils;
+import com.sogou.upd.passport.exception.ServiceException;
+import com.sogou.upd.passport.service.account.AccountSecureService;
+import com.sogou.upd.passport.service.account.generator.SecureCodeGenerator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created with IntelliJ IDEA. User: hujunfei Date: 13-5-21 Time: 上午11:52 To change this template
+ * use File | Settings | File Templates.
+ */
+@Service
+public class AccountSecureServiceImpl implements AccountSecureService {
+
+    private static final String CACHE_PREFIX_PASSPORTID_RESETPWDSECURECODE = CacheConstant.CACHE_PREFIX_PASSPORTID_RESETPWDSECURECODE;
+    private static final String CACHE_PREFIX_PASSPORTID_MODSECINFOSECURECODE = CacheConstant.CACHE_PREFIX_PASSPORTID_MODSECINFOSECURECODE;
+
+
+    private static final Logger logger = LoggerFactory.getLogger(AccountSecureServiceImpl.class);
+
+    @Autowired
+    private RedisUtils redisUtils;
+
+    @Override
+    public String getSecureCodeResetPwd(String passportId, int clientId) throws ServiceException {
+        return getSecureCode(passportId, clientId, CACHE_PREFIX_PASSPORTID_RESETPWDSECURECODE);
+    }
+
+    @Override
+    public boolean checkSecureCodeResetPwd(String passportId, int clientId, String secureCode)
+            throws ServiceException {
+        return checkSecureCode(passportId, clientId, secureCode, CACHE_PREFIX_PASSPORTID_RESETPWDSECURECODE);
+    }
+
+    @Override
+    public String getSecureCodeModSecureInfo(String passportId, int clientId) throws ServiceException {
+        return getSecureCode(passportId, clientId, CACHE_PREFIX_PASSPORTID_MODSECINFOSECURECODE);
+    }
+
+    @Override
+    public boolean checkSecureCodeModSecureInfo(String passportId, int clientId, String secureCode)
+            throws ServiceException {
+        return checkSecureCode(passportId, clientId, secureCode, CACHE_PREFIX_PASSPORTID_MODSECINFOSECURECODE);
+    }
+
+    private String getSecureCode(String passportId, int clientId, String prefix)
+            throws ServiceException {
+        String cacheKey = prefix + passportId + "_" + clientId;
+        try {
+            String secureCode = SecureCodeGenerator.generatorSecureCode(passportId, clientId);
+            redisUtils.set(cacheKey, secureCode);
+            redisUtils.expire(cacheKey, DateAndNumTimesConstant.SECURECODE_VALID);
+            return secureCode;
+        } catch (Exception e) {
+            redisUtils.delete(cacheKey);
+            throw new ServiceException(e);
+        }
+    }
+
+    private boolean checkSecureCode(String passportId, int clientId, String secureCode, String prefix)
+            throws ServiceException {
+        try {
+            String cacheKey = prefix + passportId + "_" + clientId;
+            boolean flag = false;
+            if (redisUtils.checkKeyIsExist(cacheKey) && redisUtils.get(cacheKey).equals(secureCode)) {
+                flag = true;
+            }
+            redisUtils.delete(cacheKey);
+            return flag;
+        } catch (Exception e) {
+            throw new ServiceException(e);
+        }
+    }
+}
