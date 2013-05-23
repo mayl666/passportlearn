21844d24bb9e0205b92f64de5f8960b38aa7f91e
hujunfei <hujunfei@sogou-inc.com>
2013-05-23 11:02:05 +0800

    AccountInfoService单元测试

---
+++ passport-main-service/src/test/java/com/sogou/upd/passport/service/account/AccountInfoServiceTest.java
@@ -0,0 +1,69 @@
+package com.sogou.upd.passport.service.account;
+
+import com.sogou.upd.passport.model.account.AccountInfo;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+/**
+ * Created with IntelliJ IDEA. User: hujunfei Date: 13-5-6 Time: 下午6:49 To change this template use
+ * File | Settings | File Templates.
+ */
+@ContextConfiguration(locations = "classpath:spring-config-test.xml")
+public class AccountInfoServiceTest extends AbstractJUnit4SpringContextTests {
+    private static final String PASSPORT_ID = "13552848876@sohu.com";
+    private static final String EMAIL = "Binding123@163.com";
+    private static final String NEW_EMAIL = "NewBinding123@163.com";
+    private static final String QUESTION = "Secure question";
+    private static final String NEW_QUESTION = "New secure question";
+    private static final String ANSWER = "Secure answer";
+    private static final String NEW_ANSWER = "New secure answer";
+
+    @Autowired
+    private AccountInfoService accountInfoService;
+
+    /**
+     * 测试查询是否成功
+     */
+    @Test
+    public void testQueryAccountInfoByPassportId() {
+        AccountInfo accountInfo = accountInfoService.queryAccountInfoByPassportId(PASSPORT_ID);
+        if (accountInfo != null) {
+            System.out.println("查询成功！");
+        } else {
+            System.out.println("查询失败！");
+        }
+    }
+
+    /**
+     * 测试修改
+     */
+    @Test
+    public void testModifyEmailByPassportId() {
+        AccountInfo accountInfo = accountInfoService.queryAccountInfoByPassportId(PASSPORT_ID);
+        accountInfoService.modifyEmailByPassportId(PASSPORT_ID, NEW_EMAIL);
+        AccountInfo accountInfo1 = accountInfoService.queryAccountInfoByPassportId(PASSPORT_ID);
+        if (NEW_EMAIL.equals(accountInfo1.getEmail())) {
+            System.out.println("修改成功:"+NEW_EMAIL);
+        } else {
+            System.out.println("修改失败");
+        }
+        accountInfoService.modifyEmailByPassportId(PASSPORT_ID, accountInfo.getEmail());
+    }
+
+    @Test
+    public void testModifyQuesByPassportId() {
+        AccountInfo accountInfo = accountInfoService.queryAccountInfoByPassportId(PASSPORT_ID);
+        accountInfoService.modifyQuesByPassportId(PASSPORT_ID, NEW_QUESTION, NEW_ANSWER);
+        AccountInfo accountInfo1 = accountInfoService.queryAccountInfoByPassportId(PASSPORT_ID);
+        if (NEW_QUESTION.equals(accountInfo1.getQuestion()) && NEW_ANSWER.equals(accountInfo1.getAnswer())) {
+            System.out.println("修改成功:"+accountInfo1.getQuestion() + accountInfo1.getAnswer());
+            System.out.println("原："+accountInfo.getQuestion() + accountInfo.getAnswer());
+        } else {
+            System.out.println("修改失败");
+        }
+        accountInfoService.modifyQuesByPassportId(PASSPORT_ID, accountInfo.getQuestion(), accountInfo.getAnswer());
+    }
+}
