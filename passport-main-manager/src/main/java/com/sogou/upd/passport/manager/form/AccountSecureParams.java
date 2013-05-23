3633b2cb04b1783581f8a797398bf27ad84453d2
hujunfei <hujunfei@sogou-inc.com>
2013-05-23 14:46:34 +0800

    Params类

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/form/AccountSecureParams.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/form/AccountSecureParams.java
@@ -2,53 +2,22 @@ package com.sogou.upd.passport.manager.form;
 
 import org.hibernate.validator.constraints.NotBlank;
 
+import javax.validation.constraints.Min;
+
 /**
  * Created with IntelliJ IDEA. User: hujunfei Date: 13-4-28 Time: 下午1:59 To change this template use
  * File | Settings | File Templates.
+ * 用于获取前端的常用参数
  */
 public class AccountSecureParams {
 
     @NotBlank(message = "账号不允许为空!")
     private String passport_id;
     @NotBlank(message = "client_id不允许为空!")
+    @Min(0)
     private String client_id;
     private String password;
-    private String smscode;
-    private String answer;
-    // @NotNull(message = "密保方式不允许为空!") TODO：此参数是否有必要使用?
-    private int secure_mode;
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getSmscode() {
-        return smscode;
-    }
-
-    public void setSmscode(String smscode) {
-        this.smscode = smscode;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    public int getSecure_mode() {
-        return secure_mode;
-    }
-
-    public void setSecure_mode(int secure_mode) {
-        this.secure_mode = secure_mode;
-    }
+    private String scode;
 
     public String getPassport_id() {
         return passport_id;
@@ -64,5 +33,21 @@ public class AccountSecureParams {
 
     public void setClient_id(String client_id) {
         this.client_id = client_id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getScode() {
+        return scode;
+    }
+
+    public void setScode(String scode) {
+        this.scode = scode;
     }
 }
