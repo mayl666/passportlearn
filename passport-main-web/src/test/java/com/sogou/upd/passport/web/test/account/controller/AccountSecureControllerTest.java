88ae8865d8a7534d497adac44b5de1121304c93a
hujunfei <hujunfei@sogou-inc.com>
2013-05-27 19:33:51 +0800

    测试AccountSecureController

--- passport-main-web/src/test/java/com/sogou/upd/passport/web/test/account/controller/AccountSecureControllerTest.java
+++ passport-main-web/src/test/java/com/sogou/upd/passport/web/test/account/controller/AccountSecureControllerTest.java
@@ -1,13 +1,10 @@
 package com.sogou.upd.passport.web.test.account.controller;
 
 import com.sogou.upd.passport.common.result.Result;
-import com.sogou.upd.passport.service.account.generator.PwdGenerator;
 import com.sogou.upd.passport.web.test.BaseActionTest;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -17,19 +14,7 @@ import java.util.Map;
  * Created with IntelliJ IDEA. User: hujunfei Date: 13-5-24 Time: 下午2:15 To change this template use
  * File | Settings | File Templates.
  */
-@ContextConfiguration(locations = "classpath:spring-config-test.xml")
 public class AccountSecureControllerTest extends BaseActionTest {
-
-    @Test
-    public void testLogin() throws Exception {
-        String url = "/api/login";
-        Map<String, String> params = new HashMap<>();
-        params.put("passport_id", "hujunfei1986@163.com");
-        params.put("client_id", "999");
-        params.put("password", "222222");
-        Result result = sendPostLocal(url, params);
-        System.out.println(result == null ? "Wrong": result.getStatusText());
-    }
 
     @Test
     public void testQuery() throws IOException {
@@ -69,5 +54,106 @@ public class AccountSecureControllerTest extends BaseActionTest {
         System.out.println(result.getData());
     }
 
+    @Test
+    public void testSendSms() throws Exception {
+        String url = "/api/sendsms";
+        Map<String, String> params = new HashMap<>();
+        params.put("username", "hujunfei1986@163.com");
+        params.put("client_id", "999");
+        params.put("mode", "2");
+        params.put("module", "3");
+        Result result = sendPostLocal(url, params);
+        System.out.println(result == null ? "result为空":result.getStatusText());
+    }
+
+    @Test
+    public void testCheckSmsReset() throws Exception {
+        String url = "/api/findpwd/checksms";
+        Map<String, String> params = new HashMap<>();
+        params.put("passport_id", "hujunfei1986@163.com");
+        params.put("client_id", "999");
+        params.put("smscode", "99875");
+        Result result = sendPostLocal(url, params);
+        System.out.println(result == null ? "result为空":result.getStatusText() + result.getData());
+    }
+
+    @Test
+    public void testMobileReset() throws Exception {
+        String url = "/api/findpwd/mobile";
+        Map<String, String> params = new HashMap<>();
+        params.put("passport_id", "hujunfei1986@163.com");
+        params.put("client_id", "999");
+        params.put("scode", "6647bec044bb446fd20cd186358fe173");
+        params.put("password", DigestUtils.md5Hex("111111".getBytes()));
+        Result result = sendPostLocal(url, params);
+        System.out.println(result == null ? "result为空":result.getStatusText() + result.getData());
+
+    }
+
+    @Test
+    public void testCheckAnswer() throws Exception {
+        String url = "/api/findpwd/checkanswer";
+        Map<String, String> params = new HashMap<>();
+        params.put("passport_id", "hujunfei1986@163.com");
+        params.put("client_id", "999");
+        params.put("answer", "day");
+        params.put("token", "3c81bb228cf848dcac7a664edc141040");
+        params.put("captcha", "G2XCH");
+        Result result = sendPostLocal(url, params);
+        System.out.println(result == null ? "result为空":result.getStatusText() + result.getData());
+    }
+
+    @Test
+    public void testAnswerReset() throws Exception {
+        String url = "/api/findpwd/ques";
+        Map<String, String> params = new HashMap<>();
+        params.put("passport_id", "hujunfei1986@163.com");
+        params.put("client_id", "999");
+        params.put("password", DigestUtils.md5Hex("222222".getBytes()));
+        params.put("scode", "e54cc4bc09739ade3e746ad892305227");
+        Result result = sendPostLocal(url, params);
+        System.out.println(result == null ? "result为空":result.getStatusText() + result.getData());
+    }
+
+    @Test
+    public void testSendEmailForBind() throws Exception {
+        String url = "/api/bind/sendemail";
+        Map<String, String> params = new HashMap<>();
+        params.put("passport_id", "hujunfei1986@163.com");
+        params.put("client_id", "999");
+        params.put("password", "222222");
+        params.put("new_email", "hujunfei@sogou-inc.com");
+        Result result = sendPostLocal(url, params);
+        System.out.println(result == null ? "result为空":result.getStatusText() + result.getData());
+    }
+
+
+    @Test
+    public void testSendSmsNew() throws Exception {
+        String url = "/api/sendsms";
+        Map<String, String> params = new HashMap<>();
+        params.put("username", "180XXXXXXXX"); // 填写待绑定新手机号
+        params.put("client_id", "999");
+        params.put("mode", "3");
+        params.put("module", "3");
+        Result result = sendPostLocal(url, params);
+        System.out.println(result == null ? "result为空":result.getStatusText());
+    }
+
+
+    @Test
+    public void testModifyQues() throws Exception {
+        String url = "/api/bind/ques";
+        Map<String, String> params = new HashMap<>();
+        params.put("passport_id", "hujunfei1986@163.com");
+        params.put("client_id", "999");
+        params.put("password", DigestUtils.md5Hex("111111".getBytes()));
+        params.put("new_ques", "day");
+        params.put("new_answer", "birthday");
+        Result result = sendPostLocal(url, params);
+        System.out.println(result == null ? "result为空":result.getStatusText());
+    }
+
+    // @Test
 
 }
