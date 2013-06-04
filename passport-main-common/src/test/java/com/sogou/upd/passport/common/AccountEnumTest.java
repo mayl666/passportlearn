c7980e66499113d3426792d197892049f41179f7
hujunfei <hujunfei@sogou-inc.com>
2013-06-04 11:20:33 +0800

    增加区分业务module的枚举类和单元测试

---
+++ passport-main-common/src/test/java/com/sogou/upd/passport/common/AccountEnumTest.java
@@ -0,0 +1,31 @@
+package com.sogou.upd.passport.common;
+
+import com.sogou.upd.passport.common.parameter.AccountModuleEnum;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA. User: hujunfei Date: 13-6-3 Time: 下午4:40 To change this template use
+ * File | Settings | File Templates.
+ */
+
+public class AccountEnumTest extends TestCase {
+
+    @Test
+    public void testAccountModule() {
+        AccountModuleEnum module = AccountModuleEnum.REGISTER;
+        Assert.assertTrue(module == AccountModuleEnum.REGISTER);
+        Assert.assertTrue(module != AccountModuleEnum.LOGIN);
+        Assert.assertTrue(module.getDirect().equals(AccountModuleEnum.REGISTER.getDirect()));
+        Assert.assertTrue(module.getDirect().equals("register"));
+        Assert.assertTrue(!module.getDirect().equals(AccountModuleEnum.LOGIN.getDirect()));
+        Assert.assertTrue(!module.getDirect().equals("login"));
+
+        // 测试是否能直接与字符串作连接操作
+        String joinStr = module + " can join strings";
+        System.out.println(joinStr);
+    }
+}
