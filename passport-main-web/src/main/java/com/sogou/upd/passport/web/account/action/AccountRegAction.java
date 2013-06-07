3ac65daae0f79bec8c97c0182a602c3d95689832
mayan <mayan@sogou-inc.com>
2013-06-07 17:59:33 +0800

    添加注册，登录，安全类

---
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/action/AccountRegAction.java
@@ -0,0 +1,9 @@
+package com.sogou.upd.passport.web.account.action;
+
+/**
+ * Created with IntelliJ IDEA. User: mayan Date: 13-6-7 Time: 下午5:48 To change this template use
+ * File | Settings | File Templates.
+ */
+public class AccountRegAction {
+
+}
