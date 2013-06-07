b653e92832239f59ace6140ccf3dc566757cfb3f
shipengzhi <shipengzhi@sogou-inc.com>
2013-06-07 20:14:06 +0800

    增加代理接口和代理Manager实现

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/proxy/account/impl/ProxyLoginApiManagerImpl.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/proxy/account/impl/ProxyLoginApiManagerImpl.java
@@ -3,10 +3,13 @@ package com.sogou.upd.passport.manager.proxy.account.impl;
 import com.sogou.upd.passport.common.model.httpclient.RequestModelXml;
 import com.sogou.upd.passport.common.result.APIResultSupport;
 import com.sogou.upd.passport.common.result.Result;
+import com.sogou.upd.passport.common.utils.ErrorUtil;
 import com.sogou.upd.passport.manager.proxy.BaseProxyManager;
 import com.sogou.upd.passport.manager.proxy.SHPPUrlConstant;
 import com.sogou.upd.passport.manager.proxy.account.LoginApiManager;
 import com.sogou.upd.passport.manager.proxy.account.form.AuthUserApiParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -20,14 +23,19 @@ import java.util.Map;
 @Component("proxyLoginApiManager")
 public class ProxyLoginApiManagerImpl extends BaseProxyManager implements LoginApiManager {
 
+    private static Logger log = LoggerFactory.getLogger(ProxyLoginApiManagerImpl.class);
+
     @Override
     public Result webAuthUser(AuthUserApiParams authUserParameters) {
         Result result = new APIResultSupport(false);
-        RequestModelXml requestModelXml = new RequestModelXml(SHPPUrlConstant.AUTH_USER, "info");
-        requestModelXml.addParams(authUserParameters);
-        Map resultMap = execute(requestModelXml);
-        result.setSuccess(true);
-        result.setModels(resultMap);
+        try {
+            RequestModelXml requestModelXml = new RequestModelXml(SHPPUrlConstant.AUTH_USER, "info");
+            requestModelXml.addParams(authUserParameters);
+            result = executeResult(requestModelXml);
+        } catch (Exception e) {
+            log.error("bindMobile Fail:", e);
+            result.setCode(ErrorUtil.SYSTEM_UNKNOWN_EXCEPTION);
+        }
         return result;
     }
 
