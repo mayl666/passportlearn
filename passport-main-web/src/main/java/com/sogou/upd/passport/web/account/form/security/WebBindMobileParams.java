3dcd7b5804c6bfd985bee8a6eac7563e60bf50b6
hujunfei <hujunfei@sogou-inc.com>
2013-06-09 16:18:21 +0800

    实现Web接口

---
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/form/security/WebBindMobileParams.java
@@ -0,0 +1,42 @@
+package com.sogou.upd.passport.web.account.form.security;
+
+import com.sogou.upd.passport.web.account.form.BaseWebParams;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ * Created with IntelliJ IDEA. User: hujunfei Date: 13-6-9 Time: 下午4:05 To change this template use
+ * File | Settings | File Templates.
+ */
+public class WebBindMobileParams extends BaseWebParams {
+    @NotBlank(message = "验证码不允许为空!")
+    protected String smscode;
+    @NotBlank(message = "scode不允许为空")
+    protected String scode;
+    @NotBlank(message = "新手机号不能为空")
+    protected String new_mobile;
+
+    public String getSmscode() {
+        return smscode;
+    }
+
+    public void setSmscode(String smscode) {
+        this.smscode = smscode;
+    }
+
+    public String getScode() {
+        return scode;
+    }
+
+    public void setScode(String scode) {
+        this.scode = scode;
+    }
+
+    public String getNew_mobile() {
+        return new_mobile;
+    }
+
+    public void setNew_mobile(String new_mobile) {
+        this.new_mobile = new_mobile;
+    }
+}
---
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/form/security/WebMobileParams.java
@@ -0,0 +1,22 @@
+package com.sogou.upd.passport.web.account.form.security;
+
+import com.sogou.upd.passport.web.account.form.BaseWebParams;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ * Created with IntelliJ IDEA. User: hujunfei Date: 13-6-9 Time: 下午3:56 To change this template use
+ * File | Settings | File Templates.
+ */
+public class WebMobileParams extends BaseWebParams {
+    @NotBlank(message = "手机号不允许为空!")
+    protected String new_mobile;
+
+    public String getNew_mobile() {
+        return new_mobile;
+    }
+
+    public void setNew_mobile(String new_mobile) {
+        this.new_mobile = new_mobile;
+    }
+}
