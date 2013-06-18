c60e7225ce99e351c758a9b47d575f3bf0e998ff
zhengxin <demixcn@gmail.com>
2013-06-18 11:19:08 +0800

    build project by zhengxin

--- passport-main-web/src/main/webapp/static/js/form.js
+++ passport-main-web/src/main/webapp/static/js/form.js
@@ -91,7 +91,9 @@ define(['./utils','./conf','./uuibase' , './uuiForm'] , function(utils,conf){
             $el.find('.vpic img').attr('src' , "/captcha?token="+ $el.find('.token').val() + '&t=' + +new Date());
             return false;
         });
-        
+        $el.click(function(){
+            $el.find('.form-error,.form-success').hide();;
+        });
     };
 
     return{
@@ -125,7 +127,12 @@ define(['./utils','./conf','./uuibase' , './uuiForm'] , function(utils,conf){
                     if( !onsuccess || onsuccess($el) ){
                         $.post($el.attr('action'), $el.serialize() , function(data){
                             data = utils.parseResponse(data);
-                            alert(data.statusText)
+                            
+                            if( !+data.status ){
+                                $el.find('.form-success').show().find('span').html('提交成功');
+                            }else{
+                                $el.find('.form-error').show().find('span').html(data.statusText? data.statusText : '未知错误');
+                            }
                         });
                     }
                     return false;
@@ -133,7 +140,10 @@ define(['./utils','./conf','./uuibase' , './uuiForm'] , function(utils,conf){
             });
             $el.append('<input type="hidden" name="token" value="" class="token"/>');
             $el.append('<input name="client_id" value="'+ conf.client_id +'" type="hidden"/>');
-
+            
+            $el.find('.form-btn').before('<div class="form-error"><span></span></div>');
+            $el.find('.form-btn').before('<div class="form-success"><span></span></div>');
+            
             initToken($el);
             bindOptEvent($el);
         }
