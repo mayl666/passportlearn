ef5f2dcb786c923619200c25337b58cb1a42d72b
zhengxin <demixcn@gmail.com>
2013-06-18 10:42:45 +0800

    build project by zhengxin

--- passport-main-web/src/main/webapp/static/js/ucenter.js
+++ passport-main-web/src/main/webapp/static/js/ucenter.js
@@ -15,7 +15,7 @@ define(['./common' , './tpl'] , function(common , ursa ){
         index: function(data){
             var tpl = $('#Target');
             var wrapper = tpl.parent();
-            var last_login_time = new Date(data.last_login_time);
+            var last_login_time = new Date(data.last_login);
             data.time = {
                 year: last_login_time.getFullYear(),
                 month: last_login_time.getMonth()+1,
