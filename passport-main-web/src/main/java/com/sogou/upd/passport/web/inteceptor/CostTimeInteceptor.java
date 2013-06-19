638d4b7b5a92b19c4c72bba04690fa8b7a069ae2
ligang201716 <ligang201716@sogou-inc.com>
2013-06-19 18:57:55 +0800

    性能log

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/inteceptor/CostTimeInteceptor.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/inteceptor/CostTimeInteceptor.java
@@ -6,13 +6,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.sogou.upd.passport.web.ControllerHelper;
+import org.perf4j.StopWatch;
+import org.perf4j.log4j.Log4JStopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 /**
- * 身份和安全验证拦截器
+ * 使用perf4j来监控性能
  * @author shipengzhi
  *
  */
@@ -20,10 +22,16 @@ public class CostTimeInteceptor extends HandlerInterceptorAdapter {
 
 	private static final Logger log = LoggerFactory.getLogger(CostTimeInteceptor.class);
 
+    private static final org.apache.log4j.Logger prefLogger = org.apache.log4j.Logger.getLogger("webTimingLogger");
+
+    private final static int SLOW_TIME = 500;
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		long startTime = System.currentTimeMillis();
 		request.setAttribute("startTime", startTime);
+        StopWatch stopWatch = new Log4JStopWatch(prefLogger);
+        request.setAttribute("stopWatch", stopWatch);
 		return true;
 	}
 
@@ -38,6 +46,30 @@ public class CostTimeInteceptor extends HandlerInterceptorAdapter {
 			log.info("[" + request.getRequestURI() + "] executeTime : " + executeTime + "ms, params : "+param);
 		}
 	}
+
+    @Override
+    public void afterCompletion(
+            HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        try{
+            Object stopWatchObject=request.getAttribute("stopWatch");
+            if(stopWatchObject!=null){
+                StopWatch stopWatch= (StopWatch) stopWatchObject;
+                String tag= request.getRequestURI();
+                if(stopWatch.getElapsedTime() >= SLOW_TIME){
+                    tag+="(slow)";
+                }
+                String message="success";
+                if(ex!=null){
+                    message="failed";
+                }
+                stopWatch.stop(tag, message);
+            }
+        }catch (Exception e){
+            log.error("CostTimeInteceptor.afterCompletion error",e);
+        }
+    }
+
 	
 
 }
