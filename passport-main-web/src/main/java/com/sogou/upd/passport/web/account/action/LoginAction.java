7279a11a240b96681022621651461f1dd06d2536
chengjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2013-06-20 15:01:00 +0800

    增加判断是否需要验证码接口

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/account/action/LoginAction.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/action/LoginAction.java
@@ -2,12 +2,15 @@ package com.sogou.upd.passport.web.account.action;
 
 import com.google.common.base.Strings;
 import com.sogou.upd.passport.common.LoginConstant;
+import com.sogou.upd.passport.common.parameter.AccountDomainEnum;
 import com.sogou.upd.passport.common.result.APIResultSupport;
 import com.sogou.upd.passport.common.result.Result;
 import com.sogou.upd.passport.common.utils.CookieUtils;
 import com.sogou.upd.passport.common.utils.ErrorUtil;
+import com.sogou.upd.passport.common.utils.PhoneUtil;
 import com.sogou.upd.passport.manager.account.LoginManager;
 import com.sogou.upd.passport.manager.api.SHPPUrlConstant;
+import com.sogou.upd.passport.manager.form.CheckUserNameExistParameters;
 import com.sogou.upd.passport.manager.form.WebLoginParameters;
 import com.sogou.upd.passport.web.BaseController;
 import com.sogou.upd.passport.web.ControllerHelper;
@@ -18,11 +21,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URLDecoder;
 
 /**
  *  User: mayan
@@ -38,6 +43,32 @@ public class LoginAction extends BaseController {
   @Autowired
   private LoginManager loginManager;
 
+    /**
+     * 用户注册检查用户名是否存在
+     *
+     * @param checkParam
+     */
+    @RequestMapping(value = "/login/checkNeedCaptcha", method = RequestMethod.GET)
+    @ResponseBody
+    public String checkNeedCaptcha(HttpServletRequest request,CheckUserNameExistParameters checkParam)
+            throws Exception {
+
+        Result result = new APIResultSupport(false);
+        //参数验证
+        String validateResult = ControllerHelper.validateParams(checkParam);
+        if (!Strings.isNullOrEmpty(validateResult)) {
+            result.setCode(ErrorUtil.ERR_CODE_COM_REQURIE);
+            result.setMessage(validateResult);
+            return result.toString();
+        }
+
+        String username= URLDecoder.decode(checkParam.getUsername(), "utf-8");
+        //校验是否需要验证码
+        boolean needCaptcha = loginManager.needCaptchaCheck(checkParam.getClient_id(),username,getIp(request));
+        result.setSuccess(true);
+        result.setDefaultModel("needCaptcha",needCaptcha);
+        return result.toString();
+    }
   /**
    * web页面登录
    *
