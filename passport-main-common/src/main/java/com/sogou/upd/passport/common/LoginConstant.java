90159e5f83a2c37886f38c955f0bc4bd05bb65d3
mayan <mayan@sogou-inc.com>
2013-07-02 20:29:12 +0800

    配合测试修改限制 恢复

--- passport-main-common/src/main/java/com/sogou/upd/passport/common/LoginConstant.java
+++ passport-main-common/src/main/java/com/sogou/upd/passport/common/LoginConstant.java
@@ -47,7 +47,7 @@ public class LoginConstant {
   /**
    * 一天内某一个ip注册次数限制
    */
-  public static final int REGISTER_IP_LIMITED = 4; // ip一天限制次数
+  public static final int REGISTER_IP_LIMITED = 100; // ip一天限制次数
   /**
    * 一天内某一个cookie注册次数限制
    */
@@ -61,6 +61,6 @@ public class LoginConstant {
   /**
    * 一天内某一个ip提及反馈次数限制
    */
-  public static final int ADDPROBLEM_IP_LIMITED = 100; // ip一天限制次数
+  public static final int ADDPROBLEM_IP_LIMITED = 10; // ip一天限制次数
 
 }
