16e84c6471411c143ddfb59a437fa12246f71313
shipengzhi <shipengzhi@sogou-inc.com>
2013-07-10 22:50:37 +0800

    修复T3手机验证码接口bug

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/account/api/CaptchaController.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/api/CaptchaController.java
@@ -1,17 +1,6 @@
 package com.sogou.upd.passport.web.account.api;
 
-import com.google.common.base.Strings;
-import com.sogou.upd.passport.common.result.APIResultSupport;
-import com.sogou.upd.passport.common.result.Result;
-import com.sogou.upd.passport.common.utils.ErrorUtil;
-import com.sogou.upd.passport.common.utils.RedisUtils;
-import com.sogou.upd.passport.manager.ManagerHelper;
 import com.sogou.upd.passport.manager.account.RegManager;
-import com.sogou.upd.passport.manager.api.account.RegisterApiManager;
-import com.sogou.upd.passport.manager.api.account.form.BaseMoblieApiParams;
-import com.sogou.upd.passport.manager.app.ConfigureManager;
-import com.sogou.upd.passport.web.ControllerHelper;
-import com.sogou.upd.passport.web.account.form.MoblieCodeParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,7 +15,7 @@ import java.awt.image.BufferedImage;
 import java.util.Map;
 
 /**
- * 手机验证码和 web注册时页面展示的验证码
+ * web注册时页面展示的验证码
  * User: mayan
  * Date: 13-5-7 Time: 下午6:22
  */
@@ -34,16 +23,7 @@ import java.util.Map;
 public class CaptchaController {
 
     @Autowired
-    private RedisUtils redisUtils;
-
-    @Autowired
     private RegManager regManager;
-    @Autowired
-    private ConfigureManager configureManager;
-    @Autowired
-    private RegisterApiManager proxyRegisterApiManager;
-    @Autowired
-    private RegisterApiManager sgRegisterApiManager;
 
     /**
      * web注册时页面展示的验证码
@@ -73,48 +53,6 @@ public class CaptchaController {
         return null;
     }
 
-    /**
-     * 手机账号注册时发送的验证码
-     *
-     * @param reqParams 传入的参数
-     */
-    @RequestMapping(value = {"/mobile/sendsms"}, method = RequestMethod.GET)
-    @ResponseBody
-    public Object sendMobileCode(MoblieCodeParams reqParams)
-            throws Exception {
-        Result result = new APIResultSupport(false);
-        //参数验证
-        String validateResult = ControllerHelper.validateParams(reqParams);
-        if (!Strings.isNullOrEmpty(validateResult)) {
-            result.setCode(ErrorUtil.ERR_CODE_COM_REQURIE);
-            result.setMessage(validateResult);
-            return result.toString();
-        }
-        //验证client_id
-        int clientId = Integer.parseInt(reqParams.getClient_id());
 
-        //检查client_id是否存在
-        if (!configureManager.checkAppIsExist(clientId)) {
-            result.setCode(ErrorUtil.INVALID_CLIENTID);
-            return result.toString();
-        }
-        String mobile = reqParams.getMobile();
-        //为了数据迁移三个阶段，这里需要转换下参数类
-        BaseMoblieApiParams baseMoblieApiParams = buildProxyApiParams(clientId, mobile);
-        if (ManagerHelper.isInvokeProxyApi(mobile)) {
-            result = proxyRegisterApiManager.sendMobileRegCaptcha(baseMoblieApiParams);
-        } else {
-            result = sgRegisterApiManager.sendMobileRegCaptcha(baseMoblieApiParams);
-        }
-        return result.toString();
-
-    }
-
-    private BaseMoblieApiParams buildProxyApiParams(int clientId, String mobile) {
-        BaseMoblieApiParams baseMoblieApiParams = new BaseMoblieApiParams();
-        baseMoblieApiParams.setMobile(mobile);
-        baseMoblieApiParams.setClient_id(clientId);
-        return baseMoblieApiParams;
-    }
 }
 
