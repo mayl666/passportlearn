b8cb0a87360a54683ba254a78389e6cd2441b3d4
hujunfei <hujunfei@sogou-inc.com>
2013-07-18 16:35:59 +0800

    修改日志记录标示

--- passport-main-common/src/main/java/com/sogou/upd/passport/common/utils/RedisUtils.java
+++ passport-main-common/src/main/java/com/sogou/upd/passport/common/utils/RedisUtils.java
@@ -23,11 +23,7 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Redis工具类
- * User: mayan
- * Date: 13-3-27
- * Time: 上午11:19
- * To change this template use File | Settings | File Templates.
+ * Redis工具类 User: mayan Date: 13-3-27 Time: 上午11:19 To change this template use File | Settings | File Templates.
  */
 public class RedisUtils {
 
@@ -36,12 +32,12 @@ public class RedisUtils {
 
     private static RedisTemplate redisTemplate;
 
-    private static final String ALL_REQUEST_TIMER="REDIES_ALL_REQUEST";
+    private static final String ALL_REQUEST_TIMER = "REDIES_ALL_REQUEST";
 
     /*
     * 设置缓存内容
     */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_set")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_set")
     public void set(String key, String value) throws Exception {
         try {
             ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
@@ -60,7 +56,7 @@ public class RedisUtils {
     /*
     * 设置缓存内容
     */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_set")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_setObject")
     public void set(String key, Object obj) throws Exception {
         try {
             ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
@@ -75,45 +71,18 @@ public class RedisUtils {
             }
         }
     }
-    /*
-    * 设置缓存内容
-    */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_set")
-    public void set(String key, String value,long timeout,TimeUnit timeUnit) throws Exception {
-        try {
-            ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-            valueOperations.set(key, value,timeout,timeUnit);
-        } catch (Exception e) {
-            logger.error("[Cache] set cache fail, key:" + key + " value:" + value, e);
 
-        }
-    }
-
-    /*
-    * 设置缓存内容
-    */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_set")
-    public void set(String key, Object obj,long timeout,TimeUnit timeUnit) throws Exception {
-        try {
-            ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-            valueOperations.set(key, new ObjectMapper().writeValueAsString(obj),timeout,timeUnit);
-        } catch (Exception e) {
-            logger.error("[Cache] set cache fail, key:" + key + " value:" + obj, e);
-
-        }
-    }
 
     /*
      * 设置缓存内容及有效期，单位为秒
      * TODO:是否抛出异常及如何处理
      */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_set")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_setEx")
     public void setWithinSeconds(String key, String value, long timeout) throws Exception {
         set(key, value, timeout, TimeUnit.SECONDS);
-
     }
 
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_set")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_setObjectEx")
     public void setWithinSeconds(String key, Object obj, long timeout) throws Exception {
         set(key, obj, timeout, TimeUnit.SECONDS);
     }
@@ -121,7 +90,7 @@ public class RedisUtils {
     /*
      * 设置缓存内容
     */
-    @Profiled
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_multiSet")
     public <T> void multiSet(Map<String, T> mapData) throws Exception {
         try {
             Map<String, String> objectMap = Maps.newHashMap();
@@ -148,12 +117,12 @@ public class RedisUtils {
     /*
       * 设置缓存内容
       */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_increment")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_increment")
     public long increment(String key) throws Exception {
-        long countNum=0;
+        long countNum = 0;
         try {
             ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-            countNum=valueOperations.increment(key,1);
+            countNum = valueOperations.increment(key, 1);
         } catch (Exception e) {
             logger.error("[Cache] increment fail, key:" + key, e);
             throw e;
@@ -165,7 +134,7 @@ public class RedisUtils {
     * 设置缓存内容
     * 冲突不覆盖
     */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_setNx")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_setNx")
     public boolean setNx(String cacheKey, Object obj) {
         try {
             BoundValueOperations boundValueOperation = redisTemplate.boundValueOps(cacheKey);
@@ -179,12 +148,12 @@ public class RedisUtils {
     /*
    * 根据key取缓存内容
    */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_get")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_get")
     public String get(String key) {
         try {
             ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-            String res =  valueOperations.get(key);
-            if(Strings.isNullOrEmpty(res)){
+            String res = valueOperations.get(key);
+            if (Strings.isNullOrEmpty(res)) {
                 redisMissLogger.info("get cache miss, key:" + key);
             }
             return res;
@@ -197,12 +166,12 @@ public class RedisUtils {
     /*
      * 根据key取缓存内容
     */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_multiGet")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_multiGet")
     public List<String> multiGet(List<String> keyList) {
         try {
             ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-            List<String> resList =  valueOperations.multiGet(keyList);
-            if(CollectionUtils.isEmpty(resList)){
+            List<String> resList = valueOperations.multiGet(keyList);
+            if (CollectionUtils.isEmpty(resList)) {
                 redisMissLogger.info("multiGet cache miss, key:" + keyList.toString());
             }
             return resList;
@@ -214,17 +183,13 @@ public class RedisUtils {
 
     /**
      * 根据key取对象
-     *
-     * @param cacheKey
-     * @param returnClass
-     * @return
      */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_getObject")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_getObject")
     public <T> T getObject(String cacheKey, Class returnClass) {
         try {
             String cacheStr = get(cacheKey);
             if (!Strings.isNullOrEmpty(cacheStr)) {
-              T object = (T) new ObjectMapper().readValue(cacheStr, returnClass);
+                T object = (T) new ObjectMapper().readValue(cacheStr, returnClass);
                 return object;
             }
             redisMissLogger.info("getObject cache miss, key:" + cacheKey);
@@ -238,11 +203,11 @@ public class RedisUtils {
     /*
    * 判断key是否存在
    */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_checkKeyIsExist")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_checkKeyIsExist")
     public boolean checkKeyIsExist(String key) {
         try {
-            boolean res =  redisTemplate.hasKey(key);
-            if(!res){
+            boolean res = redisTemplate.hasKey(key);
+            if (!res) {
                 redisMissLogger.info("checkKeyIsExist cache miss, key:" + key);
             }
             return res;
@@ -255,15 +220,15 @@ public class RedisUtils {
     /*
    * 获取hash中所有的映射关系
    */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_hGetAll")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_hGetAll")
     public Map<String, String> hGetAll(String cacheKey) {
         try {
             BoundHashOperations boundHashOperations = redisTemplate.boundHashOps(cacheKey);
             Map<String, String> res = boundHashOperations.entries();
-            if(MapUtils.isEmpty(res)){
+            if (MapUtils.isEmpty(res)) {
                 redisMissLogger.info("hGetAll cache miss, key:" + cacheKey);
             }
-            return  res;
+            return res;
         } catch (Exception e) {
             logger.error("[Cache] hGet All cache fail, key:" + cacheKey, e);
         }
@@ -273,7 +238,7 @@ public class RedisUtils {
     /*
     * 设置hash映射关系
     */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_hPutAll")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_hPutAll")
     public void hPutAll(String cacheKey, Map<String, String> mapData) throws Exception {
         try {
 
@@ -293,7 +258,7 @@ public class RedisUtils {
     /*
     * 设置hash映射关系
     */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_cacheKey")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_hPutAllObject")
     public <T> void hPutAllObject(String cacheKey, Map<String, T> mapData) throws Exception {
         try {
             Map<String, String> objectMap = Maps.newHashMap();
@@ -319,30 +284,26 @@ public class RedisUtils {
 
     /**
      * 记录存在则覆盖，不存在则插入
-     *
-     * @param cacheKey
-     * @param key
-     * @param value
      */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_hPut")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_hPut")
     public void hPut(String cacheKey, String key, String value) throws Exception {
         try {
             BoundHashOperations<String, String, String> boundHashOperations = redisTemplate.boundHashOps(cacheKey);
             boundHashOperations.put(key, value);
         } catch (Exception e) {
             logger.error("[Cache] hPut cache fail, cacheKey:" + cacheKey + " mapKey:" + key
-                      + " mapValue:" + value, e);
+                         + " mapValue:" + value, e);
             try {
                 delete(cacheKey);
             } catch (Exception ex) {
                 logger.error("[Cache] hPut and delete cache fail, cacheKey:" + cacheKey + " mapKey:"
-                          + key + " mapValue:" + value, e);
+                             + key + " mapValue:" + value, e);
                 throw e;
             }
         }
     }
 
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_hPut")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_hPutObject")
     public void hPut(String cacheKey, String key, Object obj) throws Exception {
         BoundHashOperations<String, String, String> boundHashOperations = redisTemplate.boundHashOps(cacheKey);
         boundHashOperations.put(key, new ObjectMapper().writeValueAsString(obj));
@@ -350,18 +311,13 @@ public class RedisUtils {
 
     /**
      * 获取hash值
-     *
-     * @param cacheKey
-     * @param key
-     * @return
      */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_hGet")
-    public String hGet(String cacheKey, String key)  {
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_hGet")
+    public String hGet(String cacheKey, String key) {
         try {
-            BoundHashOperations<String, String, String> boundHashOperations = redisTemplate.boundHashOps(
-                cacheKey);
-            String res =  boundHashOperations.get(key);
-            if(Strings.isNullOrEmpty(res)){
+            BoundHashOperations<String, String, String> boundHashOperations = redisTemplate.boundHashOps(cacheKey);
+            String res = boundHashOperations.get(key);
+            if (Strings.isNullOrEmpty(res)) {
                 redisMissLogger.info("hGet cache miss, key:" + cacheKey);
             }
             return res;
@@ -371,13 +327,13 @@ public class RedisUtils {
         return null;
     }
 
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_hGetObject")
-    public  <T> T hGetObject(String cacheKey, String key, Class returnClass) {
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_hGetObject")
+    public <T> T hGetObject(String cacheKey, String key, Class returnClass) {
         try {
             BoundHashOperations<String, String, String> boundHashOperations = redisTemplate.boundHashOps(cacheKey);
             String cacheStr = boundHashOperations.get(key);
             if (!Strings.isNullOrEmpty(cacheStr)) {
-              T object = (T) new ObjectMapper().readValue(cacheStr, returnClass);
+                T object = (T) new ObjectMapper().readValue(cacheStr, returnClass);
                 return object;
             }
             redisMissLogger.info("hGetObject cache miss, key:" + cacheKey);
@@ -389,11 +345,8 @@ public class RedisUtils {
 
     /**
      * 删除Hash键值
-     *
-     * @param cacheKey
-     * @param key
      */
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_hDelete")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_hDelete")
     public void hDelete(String cacheKey, String key) {
         try {
             BoundHashOperations<String, String, String> boundHashOperations = redisTemplate.boundHashOps(cacheKey);
@@ -405,11 +358,6 @@ public class RedisUtils {
 
     /**
      * 记录存在则不覆盖返回false，不存在则插入返回true
-     *
-     * @param cacheKey
-     * @param key
-     * @param value
-     * @return
      */
     public boolean hPutIfAbsent(String cacheKey, String key, String value) {
         try {
@@ -421,7 +369,7 @@ public class RedisUtils {
         }
     }
 
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_hIncrBy")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_hIncrBy")
     public void hIncrBy(String cacheKey, String key) {
         try {
             BoundHashOperations<String, String, String> boundHashOperations = redisTemplate.boundHashOps(cacheKey);
@@ -431,7 +379,7 @@ public class RedisUtils {
         }
     }
 
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_expire")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_expire")
     public void expire(String cacheKey, long timeout) {
         try {
             redisTemplate.expire(cacheKey, timeout, TimeUnit.SECONDS);
@@ -440,30 +388,27 @@ public class RedisUtils {
         }
     }
 
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_delete")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_delete")
     public void delete(String cacheKey) {
         redisTemplate.delete(cacheKey);
     }
 
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_multiDelete")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_multiDelete")
     public void multiDelete(Collection cacheKeyList) {
         redisTemplate.delete(cacheKeyList);
     }
+
     /**
      * Map<String,String>转换成Map<String,Object>
-     *
-     * @param mapData
-     * @param returnClass
-     * @return
      */
     public static <T> Map<String, T> strMapToObjectMap(Map<String, String> mapData, Class returnClass)
-        throws Exception {
+            throws Exception {
         Map<String, T> results = Maps.newHashMap();
         Set<String> keySet = mapData.keySet();
         for (String key : keySet) {
             String value = mapData.get(key);
             if (!Strings.isNullOrEmpty(value)) {
-              T object = (T) new ObjectMapper().readValue(value,returnClass);
+                T object = (T) new ObjectMapper().readValue(value, returnClass);
                 results.put(key, object);
             }
         }
@@ -506,11 +451,11 @@ public class RedisUtils {
         valueList.leftPush(key, value);
     }
 
-  // set add
-  public void sadd(String key, String value) {
-    SetOperations operations=redisTemplate.opsForSet();
-    operations.add(key,value);
-  }
+    // set add
+    public void sadd(String key, String value) {
+        SetOperations operations = redisTemplate.opsForSet();
+        operations.add(key, value);
+    }
 
     public Set<String> smember(String key) {
         SetOperations operations = redisTemplate.opsForSet();
@@ -526,7 +471,7 @@ public class RedisUtils {
         ListOperations<String, String> valueList = redisTemplate.opsForList();
         valueList.rightPush(key, value);
         if (maxLen <= 0) {
-            return ;
+            return;
         }
         while (valueList.size(key) > maxLen) {
             valueList.leftPop(key);
@@ -538,7 +483,7 @@ public class RedisUtils {
         ListOperations<String, String> valueList = redisTemplate.opsForList();
         valueList.leftPush(key, value);
         if (maxLen <= 0) {
-            return ;
+            return;
         }
         while (valueList.size(key) > maxLen) {
             valueList.rightPop(key);
@@ -575,7 +520,7 @@ public class RedisUtils {
     }
 
     // 查询键key的列表
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_getList")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_getList")
     public List<String> getList(String key) {
         ListOperations<String, String> valueList = redisTemplate.opsForList();
         long len = valueList.size(key);
@@ -588,7 +533,7 @@ public class RedisUtils {
         return storeList;
     }
 
-    @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_getList")
+    @Profiled(el = true, logger = "rediesTimingLogger", tag = "redies_getList")
     public <T> List<T> getList(String key, Class returnClass) {
         try {
             List<String> storeList = getList(key);
@@ -608,6 +553,34 @@ public class RedisUtils {
         } catch (Exception e) {
             logger.error("[Cache] get list for object key: " + key, e);
             return null;
+        }
+    }
+
+    /*
+    * 设置缓存内容
+    */
+    // @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_setEx")
+    private void set(String key, String value, long timeout, TimeUnit timeUnit) throws Exception {
+        try {
+            ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+            valueOperations.set(key, value, timeout, timeUnit);
+        } catch (Exception e) {
+            logger.error("[Cache] set cache fail, key:" + key + " value:" + value, e);
+
+        }
+    }
+
+    /*
+    * 设置缓存内容
+    */
+    // @Profiled(el = true,logger = "rediesTimingLogger",tag = "redies_setObjectEx")
+    private void set(String key, Object obj, long timeout, TimeUnit timeUnit) throws Exception {
+        try {
+            ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+            valueOperations.set(key, new ObjectMapper().writeValueAsString(obj), timeout, timeUnit);
+        } catch (Exception e) {
+            logger.error("[Cache] set cache fail, key:" + key + " value:" + obj, e);
+
         }
     }
 }
