f6cc906e6dadd42f6222fcb87bc52c18aa60c548
chengjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2013-07-28 13:25:20 +0800

    浏览器authtoken接口

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/account/api/PCAccountController.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/api/PCAccountController.java
@@ -1,8 +1,22 @@
 package com.sogou.upd.passport.web.account.api;
 
+import com.google.common.base.Strings;
 import com.sogou.upd.passport.common.result.APIResultSupport;
 import com.sogou.upd.passport.common.result.Result;
+import com.sogou.upd.passport.common.utils.ErrorUtil;
+import com.sogou.upd.passport.manager.account.PCAccountManager;
+import com.sogou.upd.passport.manager.api.account.LoginApiManager;
+import com.sogou.upd.passport.manager.api.account.form.CreateCookieUrlApiParams;
+import com.sogou.upd.passport.manager.form.AuthPcTokenParams;
+import com.sogou.upd.passport.web.BaseController;
+import com.sogou.upd.passport.web.ControllerHelper;
+import org.apache.http.protocol.HTTP;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +28,14 @@ import javax.servlet.http.HttpServletRequest;
  * Time: 下午7:07
  * To change this template use File | Settings | File Templates.
  */
-public class PCAccountController {
+@Controller
+public class PCAccountController extends BaseController {
+    private static final Logger logger = LoggerFactory.getLogger(PCAccountController.class);
+
+    @Autowired
+    private PCAccountManager pcAccountManager;
+    @Autowired
+    private LoginApiManager proxyLoginApiManager;
 
     @RequestMapping(value = "/act/getpairtoken")
     @ResponseBody
@@ -26,5 +47,33 @@ public class PCAccountController {
 
 
         return result.toString();
+    }
+
+    @RequestMapping(value = "/act/authtoken",method = RequestMethod.GET)
+    public String authToken(HttpServletRequest request,AuthPcTokenParams authPcTokenParams) throws Exception {
+        Result result = new APIResultSupport(false);
+        //参数验证
+        String validateResult = ControllerHelper.validateParams(authPcTokenParams);
+        if (!Strings.isNullOrEmpty(validateResult)) {
+            result.setCode(ErrorUtil.ERR_CODE_COM_REQURIE);
+            result.setMessage(validateResult);
+            return result.toString();
+        }
+        result = pcAccountManager.authToken(authPcTokenParams);
+        //重定向生成cookie
+        if(result.isSuccess()){
+            CreateCookieUrlApiParams createCookieUrlApiParams = new CreateCookieUrlApiParams();
+            createCookieUrlApiParams.setUserid(authPcTokenParams.getUserid());
+            createCookieUrlApiParams.setRu("http://profile.ie.sogou.com/?status=0");
+            Result createCookieResult  = proxyLoginApiManager.buildCreateCookieUrl(createCookieUrlApiParams);
+            if (createCookieResult.isSuccess()){
+                String setcookieUrl = createCookieResult.getModels().get("url").toString();
+                return "redirect:"+setcookieUrl;
+            } else{
+                result.setCode(ErrorUtil.ERR_CODE_CREATE_COOKIE_FAILED);
+                logger.error("authToken:createCookieUrl error");
+            }
+        }
+        return "";
     }
 }
