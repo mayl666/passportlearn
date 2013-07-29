435ea1d3d681bc432b1d567c9ba15615a8c11676
zhengxin <demixcn@gmail.com>
2013-07-29 18:47:46 +0800

    build project by zhengxin

--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -1,6 +1,6 @@
 (function(){ 
     window.PassportSC=window.PassportSC||{};
     var scr=document.createElement('script');
-    scr.src='https://account.sogou.com/static/api/sogou.js?t=2013072932';
+    scr.src='https://account.sogou.com/static/api/sogou.js?t=2013072934';
     document.body.appendChild(scr);
 })();
