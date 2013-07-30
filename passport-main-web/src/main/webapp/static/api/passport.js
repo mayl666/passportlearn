41ddaca2d3eb68d61b5522fcdb6356d3a070bf6b
shipengzhi <shipengzhi@sogou-inc.com>
2013-07-30 18:25:22 +0800

    增加sogou.js，替换原来的login.js

--- passport-main-web/src/main/webapp/static/api/passport.js
+++ passport-main-web/src/main/webapp/static/api/passport.js
@@ -1,1 +1,1 @@
-(function(){ document.write('<script type="text/javascript" src="https://account.sogou.com/static/api/login.js?t=2013071831"></script>');})();
+(function(){ document.write('<script type="text/javascript" src="https://account.sogou.com/static/api/sogou.js?t=2013072934"></script>');})();
--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -1,6 +1,6 @@
 (function(){ 
     window.PassportSC=window.PassportSC||{};
     var scr=document.createElement('script');
-    scr.src='https://account.sogou.com/static/api/login.js?t=2013071831';
+    scr.src='https://account.sogou.com/static/api/sogou.js?t=2013072934';
     document.body.appendChild(scr);
 })();
