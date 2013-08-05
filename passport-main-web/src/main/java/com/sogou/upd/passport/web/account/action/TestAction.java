a31cb852df7a292ff41d24c93b9dff000846106c
hujunfei <hujunfei@sogou-inc.com>
2013-08-05 14:33:34 +0800

    测试scribe

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/account/action/TestAction.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/action/TestAction.java
@@ -37,4 +37,22 @@ public class TestAction extends BaseController {
 
         return "testscribe";
     }
+
+    @RequestMapping(value = "/base", method = RequestMethod.POST)
+    @ResponseBody
+    public Object testPathLog(HttpServletRequest request) {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+
+        UserOperationLog
+                userOperationLog = new UserOperationLog("test_sogou@sogou.com", "/internal/account/reguser", "1120", "0", getIp(request));
+        String referer = request.getHeader("referer");
+        userOperationLog.putOtherMessage("ref", referer);
+        UserOperationLogUtil.logTest(userOperationLog);
+
+        return "testbase";
+    }
 }
