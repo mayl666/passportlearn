4015450283975f5437926f8f16046d876dd7ffc7
zhengxin <demixcn@gmail.com>
2013-08-12 11:37:37 +0800

    build project by zhengxin

--- passport-main-web/src/main/webapp/static/api/passport.js
+++ passport-main-web/src/main/webapp/static/api/passport.js
@@ -1,1 +1,10 @@
-(function(){ document.write('<script type="text/javascript" src="https://account.sogou.com/static/api/sogou.js?t=2013080801"></script>');})();
+(function(){ 
+    var passhref = '';
+    if( location.protocol.indexOf('https') != -1 ){
+        passhref = 'https://account.sogou.com/static';
+    }else{
+        passhref = '';
+    }
+
+document.write('<script type="text/javascript" src="https://account.sogou.com/static/api/sogou.js?t=2013081225"></script>');
+})();
--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -1,6 +1,6 @@
 (function(){ 
     window.PassportSC=window.PassportSC||{};
     var scr=document.createElement('script');
-    scr.src='https://account.sogou.com/static/api/sogou.js?t=2013080801';
+    scr.src='https://account.sogou.com/static/api/sogou.js?t=2013081225';
     document.body.appendChild(scr);
 })();
