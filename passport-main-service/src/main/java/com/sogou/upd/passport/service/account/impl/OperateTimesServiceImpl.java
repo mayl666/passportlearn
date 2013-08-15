a177dcb1b68ccffbea9e8906f77b8b51be5b0664
chengjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2013-08-15 12:31:43 +0800

    ip  时间限制

--- passport-main-service/src/main/java/com/sogou/upd/passport/service/account/impl/OperateTimesServiceImpl.java
+++ passport-main-service/src/main/java/com/sogou/upd/passport/service/account/impl/OperateTimesServiceImpl.java
@@ -124,7 +124,7 @@ public class OperateTimesServiceImpl implements OperateTimesService {
 
                     if (!Strings.isNullOrEmpty(ip)) {
                         String ipCacheKey = CacheConstant.CACHE_PREFIX_IP_LOGINSUCCESSNUM + ip;
-                        recordTimes(ipCacheKey, DateAndNumTimesConstant.TIME_ONEHOUR);
+                        recordTimes(ipCacheKey, DateAndNumTimesConstant.TIME_ONEDAY);
                     }
 
                 }
