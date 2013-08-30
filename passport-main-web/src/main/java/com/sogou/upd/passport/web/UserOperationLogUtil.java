49889346e9955db3cc59353ff6779af284a50cbb
hujunfei <hujunfei@sogou-inc.com>
2013-08-30 14:25:36 +0800

    测试scribe

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/UserOperationLogUtil.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/UserOperationLogUtil.java
@@ -167,14 +167,14 @@ public class UserOperationLogUtil {
 
             // Thread.sleep(60);
             start = System.currentTimeMillis();
-            // userLogger.info(log.toString());
+            userLogger.info(log.toString());
             // template.convertAndSend(log.toString());
-            connection = connectionFactory.newConnection();
+/*            connection = connectionFactory.newConnection();
             channel = connection.createChannel();
             channel.queueDeclare("passport_user_1", false, false, false, null);
             channel.basicPublish("", "passport_user_1", null, log.toString().getBytes());
             channel.queueDeclare("passport_user", false, false, false, null);
-            channel.basicPublish("", "passport_user", null, log.toString().getBytes());
+            channel.basicPublish("", "passport_user", null, log.toString().getBytes());*/
             log.append(System.currentTimeMillis() - start);
             userLoggerBase.info(log.toString()); //TODO
         } catch (Exception e) {
