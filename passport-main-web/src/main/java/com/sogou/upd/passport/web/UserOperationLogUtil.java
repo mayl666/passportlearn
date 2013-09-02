7415d67e2e323eafb78eed28b40da76d7514cb99
hujunfei <hujunfei@sogou-inc.com>
2013-09-02 17:37:02 +0800

    测试scribe

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/UserOperationLogUtil.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/UserOperationLogUtil.java
@@ -196,7 +196,7 @@ public class UserOperationLogUtil {
             channel.queueDeclare("passport_user", false, false, false, null);
             channel.basicPublish("", "passport_user", null, log.toString().getBytes());*/
             log.append(System.currentTimeMillis() - end);
-            userLoggerBase.info(log.toString()); //TODO
+            userLoggerBase.info(log.toString()+",s1:"+s1+",s2:"+s2+",s3"+s3); //TODO
         } catch (Exception e) {
             logger.error("UserOperationLogUtil.log error", e);
         }
