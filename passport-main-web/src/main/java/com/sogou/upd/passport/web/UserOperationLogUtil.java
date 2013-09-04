9e4cc6e41cbbd990b4fcce044217a3a0543feccb
chengjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2013-09-04 17:21:05 +0800

    Merge remote-tracking branch 'origin/develop' into develop
    Conflicts:
    	passport-main-web/src/main/java/com/sogou/upd/passport/web/UserOperationLogUtil.java

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/UserOperationLogUtil.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/UserOperationLogUtil.java
@@ -1,11 +1,149 @@
 package com.sogou.upd.passport.web;
 
+
+import com.google.common.base.Strings;
+
+import com.sogou.upd.passport.common.CommonConstant;
+import com.sogou.upd.passport.common.lang.StringUtil;
+import com.sogou.upd.passport.common.model.useroperationlog.UserOperationLog;
+import com.sogou.upd.passport.common.parameter.AccountDomainEnum;
+import com.sogou.upd.passport.common.parameter.AccountTypeEnum;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.perf4j.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.net.InetAddress;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
 /**
- * Created with IntelliJ IDEA.
- * User: chenjiameng
- * Date: 13-9-4
- * Time: 下午5:17
- * To change this template use File | Settings | File Templates.
+ * 用于记录用户行为的log
+ * User: ligang201716@sogou-inc.com
+ * Date: 13-6-20
+ * Time: 下午12:07
  */
 public class UserOperationLogUtil {
+
+    private static final Logger userOperationLogger = LoggerFactory.getLogger("userLoggerAsync");
+    private static final Logger userOperationLocalLogger = LoggerFactory.getLogger("userLoggerLocal");
+    private static Logger userLogger = userOperationLogger;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserOperationLogUtil.class);
+
+    private static String LOCALIP = null;
+
+    /**
+     * 修改Logger，“local”修改为只记本地，“mq/rabbitmq”修改为传RabbitMQ和本地
+     *
+     * @param loggerName local/mq/rabbitmq
+     */
+    public static void changeLogger(String loggerName) {
+        if ("local".equalsIgnoreCase(loggerName)) {
+            userLogger = userOperationLocalLogger;
+        } else if ("mq".equalsIgnoreCase(loggerName) || "rabbitmq".equalsIgnoreCase(loggerName)) {
+            userLogger = userOperationLogger;
+        } else {
+            //
+        }
+    }
+
+    /**
+     * 记录用户行为
+     *
+     * @param userOperationLog
+     */
+    public static void log(UserOperationLog userOperationLog) {
+        log(userOperationLog.getPassportId(), userOperationLog.getUserOperation(), userOperationLog.getClientId(), userOperationLog.getIp(), userOperationLog.getResultCode(), userOperationLog.getOtherMessageMap());
+    }
+
+
+    /**
+     * 用于记录log代码
+     * 日志格式：日期+时间  用户ID  用户域  用户操作  应用ID  IP地址  结果代码  响应时间  referer  附加信息
+     *
+     * @param passportId   用户id
+     * @param operation    用户执行的操作
+     * @param clientId     终端代码
+     * @param resultCode   执行结果码
+     * @param otherMessage 其它信息
+     */
+    public static void log(String passportId, String operation, String clientId, String ip, String resultCode, Map<String, String> otherMessage) {
+        try {
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+                    .getRequest();
+            if (StringUtil.isBlank(operation)) {
+                operation = request.getRequestURI();
+            }
+            StringBuilder log = new StringBuilder();
+            Date date = new Date();
+            String timestamp = String.valueOf(date.getTime()).substring(0, 10);
+            log.append(timestamp);
+            log.append(":").append(new SimpleDateFormat("HH:mm:ss").format(date));
+            log.append("\t").append(StringUtil.defaultIfEmpty(getLocalIp(request), "-"));
+            log.append("\t").append(StringUtil.defaultIfEmpty(passportId, "-").replace("\t", " "));  // 防止恶意用户调用接口输入非法用户名
+
+            AccountDomainEnum domain = AccountDomainEnum.getAccountDomain(passportId);
+            String domainStr = domain.toString();
+            if (domain == AccountDomainEnum.THIRD) {
+                AccountTypeEnum accountTypeEnum = AccountTypeEnum.getAccountType(passportId);
+                if (accountTypeEnum != AccountTypeEnum.UNKNOWN) {
+                    domainStr = accountTypeEnum.toString();
+                }
+            } else if (domain == AccountDomainEnum.INDIVID) {
+                if (operation.indexOf("connect") != -1) {
+                    domainStr = passportId;
+                } else {
+                    domainStr = AccountDomainEnum.SOGOU.toString();
+                }
+            }
+
+            log.append("\t").append(StringUtil.defaultIfEmpty(domainStr, "-"));
+            log.append("\t").append(StringUtil.defaultIfEmpty(operation, "-"));
+            log.append("\t").append(StringUtil.defaultIfEmpty(clientId, "-"));
+            log.append("\t").append(StringUtil.defaultIfEmpty(ip, "-"));
+            log.append("\t").append(StringUtil.defaultIfEmpty(resultCode, "-"));
+
+
+            Object stopWatchObject = request.getAttribute(CommonConstant.STOPWATCH);
+            if (stopWatchObject != null && stopWatchObject instanceof StopWatch) {
+                StopWatch stopWatch = (StopWatch) stopWatchObject;
+                long startTime = stopWatch.getStartTime();
+                long costTime = System.currentTimeMillis() - startTime;
+                log.append("\t").append(costTime);
+            } else {
+                log.append("\t-");
+            }
+
+            String referer = otherMessage.remove("ref");
+            log.append("\t").append(StringUtil.defaultIfEmpty(referer, "-"));
+
+            String otherMsgJson = new ObjectMapper().writeValueAsString(otherMessage).replace("\t", " ");
+            log.append("\t").append(otherMsgJson);
+
+            userLogger.info(log.toString());
+        } catch (Exception e) {
+            logger.error("UserOperationLogUtil.log error", e);
+        }
+
+
+    }
+
+    private static String getLocalIp(HttpServletRequest request) {
+        try {
+            if (Strings.isNullOrEmpty(LOCALIP)) {
+                LOCALIP = InetAddress.getLocalHost().getHostAddress();
+            }
+        } catch (Exception e) {
+            LOCALIP = request.getLocalAddr();
+        }
+
+        return LOCALIP;
+    }
 }
