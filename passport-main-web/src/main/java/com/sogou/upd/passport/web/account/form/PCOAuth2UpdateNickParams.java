a2c9e2785268e6fb9ecfb8972d11df58876fe01a
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2013-09-10 18:54:54 +0800

    页面调整

---
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/form/PCOAuth2UpdateNickParams.java
@@ -0,0 +1,43 @@
+package com.sogou.upd.passport.web.account.form;
+
+import com.sogou.upd.passport.common.CommonConstant;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ * sohu+浏览器个人中心页
+ * User: shipengzhi
+ * Date: 13-7-24
+ * Time: 下午11:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class PCOAuth2UpdateNickParams {
+    @NotBlank(message = "accesstoken illegal")
+    private String accesstoken="";   //获取的访问token
+    @NotBlank(message = "昵称不能为空")
+    private String nick;
+
+    private String sname = "";  //账号，sohu+继承而来，这里不做处理
+    public String getAccesstoken() {
+        return accesstoken;
+    }
+
+    public void setAccesstoken(String accesstoken) {
+        this.accesstoken = accesstoken;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+}
