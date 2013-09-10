05658df073ef44564e720ba4068476c32fc216c1
chengjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2013-09-10 14:58:41 +0800

    sohu+

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/account/api/PCOAuth2AccountController.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/api/PCOAuth2AccountController.java
@@ -1,11 +1,117 @@
 package com.sogou.upd.passport.web.account.api;
 
+import com.google.common.base.Strings;
+import com.sogou.upd.passport.common.result.APIResultSupport;
+import com.sogou.upd.passport.common.result.Result;
+import com.sogou.upd.passport.common.utils.ErrorUtil;
+import com.sogou.upd.passport.manager.account.OAuth2AuthorizeManager;
+import com.sogou.upd.passport.manager.api.account.UserInfoApiManager;
+import com.sogou.upd.passport.manager.api.account.form.GetUserInfoApiparams;
+import com.sogou.upd.passport.manager.app.ConfigureManager;
+import com.sogou.upd.passport.model.app.AppConfig;
+import com.sogou.upd.passport.oauth2.authzserver.request.OAuthTokenASRequest;
+import com.sogou.upd.passport.oauth2.common.exception.OAuthProblemException;
+import com.sogou.upd.passport.web.BaseController;
+import com.sogou.upd.passport.web.ControllerHelper;
+import com.sogou.upd.passport.web.account.form.PCOAuth2IndexParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
+ * sohu+浏览器相关接口替换
  * Created with IntelliJ IDEA.
  * User: chenjiameng
- * Date: 13-9-10
- * Time: 下午2:55
+ * Date: 13-9-9
+ * Time: 下午7:37
  * To change this template use File | Settings | File Templates.
  */
-public class PCOAuth2AccountController {
+@Controller
+@RequestMapping("/oauth2")
+public class PCOAuth2AccountController extends BaseController {
+    private static final Logger logger = LoggerFactory.getLogger(PCOAuth2AccountController.class);
+    @Autowired
+    private UserInfoApiManager proxyUserInfoApiManagerImpl;
+
+    @RequestMapping(value = "/pclogin", method = RequestMethod.GET)
+    public String pcLogin(Model model) throws Exception {
+        return "";
+    }
+
+    @RequestMapping(value = "/oauth2/pcindex", method = RequestMethod.GET)
+    public String pcindex(HttpServletRequest request, PCOAuth2IndexParams oauth2PcIndexParams, Model model) throws Exception {
+        //参数验证
+        String validateResult = ControllerHelper.validateParams(oauth2PcIndexParams);
+        if (!Strings.isNullOrEmpty(validateResult)) {
+            return "forward:/oauth2/errorMsg?msg=" + validateResult;
+        }
+        //TODO 校验token,获取userid
+        String passportId = "tinkame700@sogou.com";
+
+        //获取昵称
+        GetUserInfoApiparams getUserInfoApiparams = new GetUserInfoApiparams(passportId, "uniqname");
+        Result getUserInfoResult = proxyUserInfoApiManagerImpl.getUserInfo(getUserInfoApiparams);
+        String uniqname;
+        if (getUserInfoResult.isSuccess()) {
+            uniqname = (String) getUserInfoResult.getModels().get("uniqname");
+            uniqname = Strings.isNullOrEmpty(uniqname) ? defaultUniqname(passportId) : uniqname;
+        } else {
+            uniqname = defaultUniqname(passportId);
+        }
+        model.addAttribute("userid", passportId);
+        model.addAttribute("uniqname", uniqname);
+
+
+
+        return "/oauth2pc/pcindex";
+    }
+
+    @RequestMapping(value = "/errorMsg")
+    @ResponseBody
+    public Object errorMsg(@RequestParam("msg") String msg) throws Exception {
+        return msg;
+    }
+
+    @Autowired
+    private OAuth2AuthorizeManager oAuth2AuthorizeManager;
+
+    @Autowired
+    private ConfigureManager configureManager;
+
+    @RequestMapping(value = "/token")
+    @ResponseBody
+    public Object authorize(HttpServletRequest request) throws Exception {
+        OAuthTokenASRequest oauthRequest;
+        Result result = new APIResultSupport(false);
+        try {
+            oauthRequest = new OAuthTokenASRequest(request);
+        } catch (OAuthProblemException e) {
+            result.setCode(e.getError());
+            result.setMessage(e.getDescription());
+            return result.toString();
+        }
+
+        int clientId = oauthRequest.getClientId();
+
+        // 检查client_id和client_secret是否有效
+        AppConfig appConfig = configureManager.verifyClientVaild(clientId, oauthRequest.getClientSecret());
+        if (appConfig == null) {
+            result.setCode(ErrorUtil.INVALID_CLIENT);
+            return result.toString();
+        }
+        result = oAuth2AuthorizeManager.oauth2Authorize(oauthRequest,appConfig);
+
+        return result.toString();
+    }
+    private String defaultUniqname(String passportId) {
+        return passportId.substring(0, passportId.indexOf("@"));
+    }
 }
