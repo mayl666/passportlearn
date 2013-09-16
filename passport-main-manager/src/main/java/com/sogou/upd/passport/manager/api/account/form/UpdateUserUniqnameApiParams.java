031cf594aa87d8476a40046a1452baa904a41a82
liuling <liuling@sogou-inc.com>
2013-09-16 21:16:15 +0800

    develop分支uniqname的验证添加非空判断，暂时还没抽了一个util方法

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/api/account/form/UpdateUserUniqnameApiParams.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/api/account/form/UpdateUserUniqnameApiParams.java
@@ -1,5 +1,6 @@
 package com.sogou.upd.passport.manager.api.account.form;
 
+import com.google.common.base.Strings;
 import com.sogou.upd.passport.common.validation.constraints.UniqName;
 import com.sogou.upd.passport.manager.api.BaseApiParams;
 import org.hibernate.validator.constraints.NotBlank;
@@ -20,6 +21,9 @@ public class UpdateUserUniqnameApiParams extends BaseApiParams {
 
     @AssertTrue(message = "昵称长度不符合规则，长度应该在2——12字符之间")
     private boolean isCheckLength() {
+        if(Strings.isNullOrEmpty(uniqname)){
+            return true;
+        }
         if (!(uniqname.length() >= 2 && uniqname.length() <= 12)) {
             return false;
         }
@@ -28,6 +32,9 @@ public class UpdateUserUniqnameApiParams extends BaseApiParams {
 
     @AssertTrue(message = "昵称中不能含有搜狐，sohu，souhu，搜狗，sogou，sougou字样")
     private boolean isCheckSensitive() {
+        if(Strings.isNullOrEmpty(uniqname)){
+            return true;
+        }
         String regx = "^(?!.*搜狗)(?!.*sogou)(?!.*sougou)(?!.*搜狐)(?!.*sohu)(?!.*souhu)(?!.*搜狐微博)(?!_)(?!.*?_$)[a-zA-Z0-9_\\u4e00-\\u9fa5]+$";
         if (!uniqname.matches(regx)) {
             return false;
@@ -37,6 +44,9 @@ public class UpdateUserUniqnameApiParams extends BaseApiParams {
 
     @AssertTrue(message = "昵称不符合组成规则，只能使用中文、字母、数字和下划线组合，但不能以下划线开头或结尾")
     private boolean isCheckElement() {
+        if(Strings.isNullOrEmpty(uniqname)){
+            return true;
+        }
         String regx = "^[a-zA-Z0-9\\u4e00-\\u9fa5]+$";
         //返回false，说明不符合组成规则
         if (!uniqname.matches(regx)) {
