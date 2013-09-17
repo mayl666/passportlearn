36f8906d0552f72260acfc72ffffb997d9bf9b1e
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2013-09-17 15:37:17 +0800

    通过token获取passportId clientid改为1044

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/account/form/PCOAuth2IndexParams.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/form/PCOAuth2IndexParams.java
@@ -19,7 +19,7 @@ public class PCOAuth2IndexParams {
     private String h;
     private String r;
     private String v; //浏览器版本
-    private int client_id= CommonConstant.BROWSER_CLIENTID;
+    private int client_id= CommonConstant.PC_CLIENTID;
 
     public String getAccesstoken() {
         return accesstoken;
