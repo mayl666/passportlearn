7d592007bbc1e38ac0096c6219b663916c5ceb38
yinyong <yinyong@sogou-inc.com>
2013-09-18 17:53:02 +0800

    Fixed sohu+ nick

--- passport-main-web/src/main/webapp/static/js/oauth2pc/app/profile.js
+++ passport-main-web/src/main/webapp/static/js/oauth2pc/app/profile.js
@@ -864,10 +864,10 @@ define(['jquery', 'lib/md5', 'app/dialog', 'lib/placeholder', 'lib/base64', 'lib
                 legal: true
             },
             nick: {
-                errMsg: '用户名为4-16位中英文字符，及“-”和“_”',
+                errMsg: '用户名为6-16位中英文字符，及“-”和“_”',
                 emptyMsg: '请填写用户名',
                 nullable: false,
-                regStr: /^[a-z][a-zA-Z0-9-_\u4e00-\u9fa5]{3,15}$/,
+                regStr: /^[a-z][a-zA-Z0-9-_\u4e00-\u9fa5]{5,15}$/,
                 legal: true
             }
         }
