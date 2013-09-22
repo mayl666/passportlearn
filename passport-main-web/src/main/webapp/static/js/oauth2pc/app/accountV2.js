fd33b074066cb9ceb41fbe2f900ab84a028a3650
yinyong <yinyong@sogou-inc.com>
2013-09-22 17:59:53 +0800

    fixed accesstoken

--- passport-main-web/src/main/webapp/static/js/oauth2pc/app/accountV2.js
+++ passport-main-web/src/main/webapp/static/js/oauth2pc/app/accountV2.js
@@ -752,7 +752,7 @@ define(['jquery','lib/utils', 'lib/placeholder'], function($, utils) {
                 switch (true) {
                     case (0 == code):
                         var data = result.data;
-                        var msg = (data.logintype || 'sogou') + '|' + data.result + '|' + data.accessToken + '|' + data.refreshToken + '|' + /*sname*/ (data.sname||data.uniqname)+ '|' + /*nick*/ data.nick + '|' + data.sid + '|' + data.passport + '|' + '1';
+                        var msg = (data.logintype || 'sogou') + '|' + data.result + '|' + data.accesstoken + '|' + data.refreshtoken + '|' + /*sname*/ (data.sname||data.uniqname)+ '|' + /*nick*/ data.nick + '|' + data.sid + '|' + data.passport + '|' + '1';
                         console.log('logintype | result | accToken | refToken | sname | nick | 是否公用电脑 | 是否自动登陆 | 是否保存\n ' + msg)
                         window.external && window.external.passport && window.external.passport('result', msg)
                         break;
