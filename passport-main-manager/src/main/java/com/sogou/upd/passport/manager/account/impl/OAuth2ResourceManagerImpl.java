0d2f20a2f9abc22a27905812936e0843768bfd37
shipengzhi <shipengzhi@sogou-inc.com>
2013-09-23 22:02:38 +0800

    修改/resource的full.get返回结果

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/account/impl/OAuth2ResourceManagerImpl.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/account/impl/OAuth2ResourceManagerImpl.java
@@ -179,6 +179,10 @@ public class OAuth2ResourceManagerImpl implements OAuth2ResourceManager {
                 data.put("large_avatar", largeAvatar);
                 data.put("mid_avatar", midAvatar);
                 data.put("tiny_avatar", tinyAvatar);
+//                data.put("update_at", System.currentTimeMillis());
+//                data.put("active_at", System.currentTimeMillis());
+//                data.put("create_at", 0);
+                data.put("status", 2);
                 resourceMap.put("data", data);
                 resourceMap.put("msg", "get full user info success");
                 resourceMap.put("code", "0");
