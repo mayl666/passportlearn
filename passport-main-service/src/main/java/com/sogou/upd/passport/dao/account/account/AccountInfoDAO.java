e1ac3c08ef3f792ef85858e238d655218f4febf0
mayan <mayan@sogou-inc.com>
2013-10-14 16:56:44 +0800

    添加个人资料

---
+++ passport-main-service/src/main/java/com/sogou/upd/passport/dao/account/account/AccountInfoDAO.java
@@ -0,0 +1,95 @@
+package com.sogou.upd.passport.dao.account.account;
+
+import com.sogou.upd.passport.model.account.AccountInfo;
+import net.paoding.rose.jade.annotation.DAO;
+import net.paoding.rose.jade.annotation.SQL;
+import net.paoding.rose.jade.annotation.SQLParam;
+import org.springframework.dao.DataAccessException;
+
+/**
+ * Created with IntelliJ IDEA. User: hujunfei Date: 13-4-25 Time: 下午4:26 To change this template use
+ * File | Settings | File Templates.
+ */
+@DAO
+public interface AccountInfoDAO {
+
+    /**
+     * 对应数据库表名称
+     */
+    String TABLE_NAME = " account_info ";
+
+    /**
+     * 所有字段列表
+     */
+    String ALL_FIELD = " id, passport_id, email, question, answer ";
+
+    /**
+     * 值列表
+     */
+    String VALUE_FIELD = " :accountInfo.id, :accountInfo.passportId, :accountInfo.email, :accountInfo.question, :accountInfo.answer ";
+
+    /**
+     * 修改字段列表
+     */
+    String UPDATE_FIELD = " passport_id = :accountInfo.passportId, email = :accountInfo.email, question = :accountInfo.question, answer = :accountInfo.answer ";
+
+    /**
+     * 根据passportId获取AccountInfo
+     */
+    @SQL("select " +
+            ALL_FIELD +
+            " from " +
+            TABLE_NAME +
+            " where passport_id=:passport_id")
+    public AccountInfo getAccountInfoByPassportId(@SQLParam("passport_id") String passport_id) throws
+            DataAccessException;
+
+    /**
+     * 根据passportId删除用户的AccountInfo信息，
+     * 内部调试接口使用
+     */
+    @SQL("delete from " +
+            TABLE_NAME +
+            " where passport_id=:passport_id")
+    public int deleteAccountInfoByPassportId(@SQLParam("passport_id") String passport_id) throws
+            DataAccessException;
+
+    /**
+     * 修改绑定邮箱，若passport_id不存在则插入新记录
+     */
+    @SQL(
+            "insert into " +
+                    TABLE_NAME +
+                    "(passport_id, email, question, answer) "
+            + "values(:passport_id,:accountInfo.email,:accountInfo.question,:accountInfo.answer) on duplicate key "
+            + "update email = :accountInfo.email")
+    public int saveEmailOrInsert(@SQLParam("passport_id") String passport_id,
+                                 @SQLParam("accountInfo") AccountInfo account_info)
+            throws DataAccessException;
+
+    /**
+     * 修改密保问题和答案，若passport_id不存在则插入新记录
+     */
+    @SQL(
+            "insert into " +
+                    TABLE_NAME +
+                    "(passport_id, email, question, answer)"
+            + "values(:passport_id,:accountInfo.email,:accountInfo.question,:accountInfo.answer) on duplicate key "
+            + "update question = :accountInfo.question, answer = :accountInfo.answer")
+    public int saveQuesOrInsert(@SQLParam("passport_id") String passport_id,
+                                @SQLParam("accountInfo") AccountInfo account_info)
+            throws DataAccessException;
+
+    /**
+     * 插入新记录
+     */
+    @SQL(
+            "insert into " +
+                    TABLE_NAME +
+                    "(passport_id,email,question,answer) "
+            + "values (:passport_id,:accountInfo.email,:accountInfo.question,:accountInfo.answer)")
+    public int insertAccountInfo(@SQLParam("passport_id") String passport_id,
+                                 @SQLParam("accountInfo") AccountInfo account_info)
+            throws DataAccessException;
+
+}
