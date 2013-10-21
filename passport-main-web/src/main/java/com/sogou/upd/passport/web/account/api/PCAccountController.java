d507f7cd8c3f28a9818f91a9b5e7fdab2f128164
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2013-10-22 00:58:39 +0800

    参数类型更改

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/account/api/PCAccountController.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/api/PCAccountController.java
@@ -248,7 +248,7 @@ public class PCAccountController extends BaseController {
             CreateCookieUrlApiParams createCookieUrlApiParams = new CreateCookieUrlApiParams();
             createCookieUrlApiParams.setUserid(userId);
             createCookieUrlApiParams.setRu(authPcTokenParams.getRu());
-            if (Integer.parseInt(authPcTokenParams.getLivetime()) > 0) {
+            if (!"0".equals(authPcTokenParams.getLivetime())) {
                 createCookieUrlApiParams.setPersistentcookie(1);
             }
             createCookieUrlApiParams.setDomain("sogou.com");
