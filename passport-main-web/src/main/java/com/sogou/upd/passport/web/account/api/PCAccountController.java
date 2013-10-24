8c568c678926142679412ecdb7f43bef21167460
shipengzhi <shipengzhi@sogou-inc.com>
2013-10-24 10:34:04 +0800

    回滚修改缓存redirectview的情况

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/account/api/PCAccountController.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/api/PCAccountController.java
@@ -232,10 +232,9 @@ public class PCAccountController extends BaseController {
     public String authToken(HttpServletRequest request, HttpServletResponse response, PcAuthTokenParams authPcTokenParams, RedirectAttributes redirectAttributes) throws Exception {
         //参数验证
         String validateResult = ControllerHelper.validateParams(authPcTokenParams);
-        redirectAttributes.addAttribute("authtokenRedirectUrl", authPcTokenParams.getRu());
         if (!Strings.isNullOrEmpty(validateResult)) {
             if (!Strings.isNullOrEmpty(authPcTokenParams.getRu())) {
-                return "redirect:{authtokenRedirectUrl}?status=1";   //status=1表示参数错误
+                return "redirect:"+authPcTokenParams.getRu() +"?status=1";   //status=1表示参数错误
             }
             return "forward:/act/errorMsg?msg=Error: parameter error!";
         }
@@ -269,11 +268,10 @@ public class PCAccountController extends BaseController {
                 response.addHeader("Sohupp-Cookie", "ppinf,pprdig");     // 输入法Mac版需要此字段
             }
             String redirectUrl = (String) getCookieValueResult.getModels().get("redirectUrl");
-            redirectAttributes.addAttribute("authtokenRedirectUrl", redirectUrl);
-            return "redirect:{authtokenRedirectUrl}";
+            return "redirect:" + redirectUrl;
         }
         //token验证失败
-        return "redirect:{authtokenRedirectUrl}?status=6";//status=6表示验证失败
+        return "redirect:"+authPcTokenParams.getRu() +"?status=6";//status=6表示验证失败
     }
 
     @RequestMapping(value = "/act/errorMsg")
