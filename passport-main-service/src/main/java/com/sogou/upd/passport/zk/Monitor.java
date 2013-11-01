9b9f6664d95c0f740be28369e5b9cd1aa9fc6286
ligang201716 <ligang201716@sogou-inc.com>
2013-11-01 12:16:38 +0800

    gzip

--- passport-main-service/src/main/java/com/sogou/upd/passport/zk/Monitor.java
+++ passport-main-service/src/main/java/com/sogou/upd/passport/zk/Monitor.java
@@ -29,7 +29,6 @@ public class Monitor {
                 .connectString(zks)
                 .connectionTimeoutMs(5000)
                 .retryPolicy(new RetryNTimes(Integer.MAX_VALUE, 10000))
-                .compressionProvider(null)
                 .build();
         curatorFramework.start();
         log.info("zookeeper monitor inti success");
