054c19b64bd919ab68d973bc873d6dd8c4b24b85
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2013-11-06 21:46:27 +0800

    删除log  大小写问题

--- passport-main-common/src/main/java/com/sogou/upd/passport/common/parameter/AccountDomainEnum.java
+++ passport-main-common/src/main/java/com/sogou/upd/passport/common/parameter/AccountDomainEnum.java
@@ -97,11 +97,13 @@ public enum AccountDomainEnum {
 
     //获取内部大小写的处理方式：第三方除外，其他账号都按小写处理
     public static String  getInternalCase(String userId){
-        String internalCaseUserId = userId;
-        if (AccountDomainEnum.THIRD != AccountDomainEnum.getAccountDomain(userId)) {
-            internalCaseUserId = userId.toLowerCase();
+        if (userId.endsWith("@focus.cn")){
+           return userId;
         }
-        return internalCaseUserId;
+        if(AccountDomainEnum.THIRD == AccountDomainEnum.getAccountDomain(userId)){
+           return userId;
+        }
+        return userId.toLowerCase();
     }
 
     @Override
