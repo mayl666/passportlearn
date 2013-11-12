6fff5e7930812bce9f5ef6f7effa4fe3c680a2ad
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2013-11-12 18:33:24 +0800

    压测代码

---
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/internal/debug/TokenRedisperfController.java
@@ -0,0 +1,11 @@
+package com.sogou.upd.passport.web.internal.debug;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenjiameng
+ * Date: 13-11-12
+ * Time: 下午6:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class TokenRedisperfController {
+}
