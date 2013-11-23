473eb95fc5dc411691538aafea4667ce04a1e4bd
yinyong <yinyong@sogou-inc.com>
2013-11-23 16:04:07 +0800

    fixed form submit

--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -7,6 +7,6 @@
     }else{
         passhref = 'http://s.account.sogou.com/u';
     }
-    scr.src= passhref + '/api/sogou.js?t=2013112327';
+    scr.src= passhref + '/api/sogou.js?t=2013112355';
     document.body.appendChild(scr);
 })();
