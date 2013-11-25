1038b211013400a99ac1bb21c561020e92471062
yinyong <yinyong@sogou-inc.com>
2013-11-25 16:35:43 +0800

    Fixed avatar url

--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -7,6 +7,6 @@
     }else{
         passhref = 'http://s.account.sogou.com/u';
     }
-    scr.src= passhref + '/api/sogou.js?t=2013112507';
+    scr.src= passhref + '/api/sogou.js?t=2013112531';
     document.body.appendChild(scr);
 })();
