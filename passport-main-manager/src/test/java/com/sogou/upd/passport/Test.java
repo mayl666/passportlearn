a2bd2440e78c4ae7050bdd10c0291289c078c71c
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2013-11-26 13:58:02 +0800

    sohu+ test用例

--- passport-main-manager/src/test/java/com/sogou/upd/passport/Test.java
+++ passport-main-manager/src/test/java/com/sogou/upd/passport/Test.java
@@ -3,6 +3,8 @@ package com.sogou.upd.passport;
 import com.sogou.upd.passport.common.math.Coder;
 import com.sogou.upd.passport.manager.ManagerHelper;
 
+import java.security.MessageDigest;
+
 /**
  * Created with IntelliJ IDEA.
  * User: chenjiameng
@@ -11,12 +13,15 @@ import com.sogou.upd.passport.manager.ManagerHelper;
  * To change this template use File | Settings | File Templates.
  */
 public class Test {
+    public static final String KEY_MD5 = "MD5";
+    private static char[] HEXCHAR = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
     public static void  main(String args[]) throws Exception{
         long ct = System.currentTimeMillis();
         System.out.println("ct:" + ct);
 //        String ct =  "1381915491000";
         String token ="7faada06773b30155f7eb93955845dfb";
-        String code = ManagerHelper.generatorCodeGBK(token, 1115, "RBCqf6a448Wj5a8#KF&POL75*5GBQ5", ct);
+        String code = generatorCodeGBK(token, 1115, "RBCqf6a448Wj5a8#KF&POL75*5GBQ5", ct);
         System.out.println("code:" + code);
 
         try {
@@ -26,5 +31,35 @@ public class Test {
             e.printStackTrace();
         }
 
+    }
+
+    public static String generatorCodeGBK(String token, int clientId, String secret, long ct) {
+        //计算默认的code
+        String code = "";
+        try {
+            code = token + clientId + secret + ct;
+            code = encryptMD5GBK(code);
+        } catch (Exception e) {
+        }
+        return code;
+    }
+
+    public static String encryptMD5GBK(String data) throws Exception {
+
+        MessageDigest md5 = MessageDigest.getInstance(KEY_MD5);
+
+        md5.update(data.getBytes("GBK"));
+
+        return toHexString(md5.digest());
+
+    }
+
+    public static String toHexString(byte[] b) {
+        StringBuilder sb = new StringBuilder(b.length * 2);
+        for (byte by : b) {
+            sb.append(HEXCHAR[(by & 0xf0) >>> 4]);
+            sb.append(HEXCHAR[by & 0x0f]);
+        }
+        return sb.toString();
     }
 }
