fdcd1de73d066fb0c1ba8d091e3165d59c8d14d1
yinyong <yinyong@sogou-inc.com>
2013-12-02 15:23:11 +0800

    fixed act

--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -7,6 +7,6 @@
     }else{
         passhref = 'http://s.account.sogou.com/u';
     }
-    scr.src= passhref + '/api/sogou.js?t=2013120236';
+    scr.src= passhref + '/api/sogou.js?t=2013120259';
     document.body.appendChild(scr);
 })();
