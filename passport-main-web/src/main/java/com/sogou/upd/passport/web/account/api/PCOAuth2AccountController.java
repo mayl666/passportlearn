1af656d61b243116a8fa916f0b06fd2fdd683687
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2013-12-03 01:41:44 +0800

    获取sohu cookie

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/account/api/PCOAuth2AccountController.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/api/PCOAuth2AccountController.java
@@ -390,8 +390,8 @@ public class PCOAuth2AccountController extends BaseController {
         }
         //生成cookie
         CookieApiParams cookieApiParams = new CookieApiParams();
-        String utfUserId = URLEncoder.encode(passportId,"utf-8");
-        cookieApiParams.setUserid(utfUserId);
+//        String utfUserId = URLEncoder.encode(passportId,"utf-8");
+        cookieApiParams.setUserid(passportId);
         cookieApiParams.setClient_id(oauth2PcIndexParams.getClient_id());
         cookieApiParams.setRu("https://account.sogou.com");
         cookieApiParams.setTrust(CookieApiParams.IS_ACTIVE);
