d70acd3dc532e678ec417a8d52618dbdc8a3f0ab
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2013-12-06 13:59:42 +0800

    增加返回sid

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/connect/ConnectCallbackController.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/connect/ConnectCallbackController.java
@@ -68,6 +68,7 @@ public class ConnectCallbackController extends BaseConnectController {
                 model.addAttribute("refreshtoken", result.getModels().get("refreshtoken"));
                 model.addAttribute("nick", result.getModels().get("nick"));
 //                model.addAttribute("sname", result.getModels().get("sname"));
+                model.addAttribute("sid", result.getModels().get("sid"));
                 model.addAttribute("passport", result.getModels().get("passport"));
                 model.addAttribute("result", 0);
                 model.addAttribute("logintype", result.getModels().get("logintype"));
