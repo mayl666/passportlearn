f372ce8935adec947d093a37c5571d00b35d6d32
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2013-12-17 15:05:13 +0800

    userId改回非encode

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/api/account/impl/ProxyLoginApiManagerImpl.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/api/account/impl/ProxyLoginApiManagerImpl.java
@@ -86,7 +86,7 @@ public class ProxyLoginApiManagerImpl extends BaseProxyManager implements LoginA
                 shUrl = SHPPUrlConstant.HTTPS_SET_COOKIE;
             }
             RequestModel requestModel = new RequestModel(shUrl);
-            requestModel.addParam("userid", Coder.encode(userId,"utf-8"));
+            requestModel.addParam("userid",userId);
             requestModel.addParam("appid", SHPPUrlConstant.APP_ID);
             requestModel.addParam("ct", ct);
             requestModel.addParam("code", code);
