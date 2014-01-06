288b0f2b4f6de563fb75dcf996e2ab60cb0f0cb9
yinyong <yinyong@sogou-inc.com>
2014-01-06 18:00:16 +0800

    update 2013 to 2014

--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -7,6 +7,6 @@
     }else{
         passhref = 'http://s.account.sogou.com/u';
     }
-    scr.src= passhref + '/api/sogou.js?t=2014010612';
+    scr.src= passhref + '/api/sogou.js?t=2014010605';
     document.body.appendChild(scr);
 })();
