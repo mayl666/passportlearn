8daa6cb77cf7138023987b06ce33c8c2f27e0384
liuling <liuling@sogou-inc.com>
2014-01-08 20:32:43 +0800

    QQ点亮http改成https请求方式

--- passport-oauth2/src/main/java/com/sogou/upd/passport/oauth2/common/utils/qqutils/OpenApiV3.java
+++ passport-oauth2/src/main/java/com/sogou/upd/passport/oauth2/common/utils/qqutils/OpenApiV3.java
@@ -74,11 +74,6 @@ public class OpenApiV3 {
             params.remove("sig");
             // 添加固定参数
             params.put("appid", this.appid);
-            // 签名密钥
-            String secret = this.appkey + "&";
-            // 计算签名
-            String sig = SnsSigCheck.makeSig(method, scriptName, params, secret);
-            params.put("sig", sig);
             StringBuilder sb = new StringBuilder(64);
             sb.append(protocol).append("://").append(this.serverName).append(scriptName);
             String url = sb.toString();
@@ -90,13 +85,10 @@ public class OpenApiV3 {
             Map map = ConnectHttpClient.executeBean(requestModel, HttpTransformat.json, Map.class);
             resp = JacksonJsonMapperUtil.getMapper().writeValueAsString(map);
         } catch (IOException ioe) {
-            logger.error("Transfer Map To Json Is Failed :", ioe);
+            logger.error("api:Transfer Map To Json Is Failed :", ioe);
             throw new IOException("Transfer Map To Json Is Failed:", ioe);
-        } catch (RuntimeException re) {
-            logger.error("http request error :", re);
-            throw new RuntimeException("http request error ", re);
         } catch (Exception e) {
-            logger.error("Execute Api Is Failed :", e);
+            logger.error("api:Execute Api Is Failed :", e);
             throw new Exception("Execute Api Is Failed:", e);
         }
         return resp;
