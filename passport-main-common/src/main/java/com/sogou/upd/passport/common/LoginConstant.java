9d08c7c00a58533ac4ecfebdabf3d588aecca90b
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2014-01-09 15:03:42 +0800

    次数修改50,500

--- passport-main-common/src/main/java/com/sogou/upd/passport/common/LoginConstant.java
+++ passport-main-common/src/main/java/com/sogou/upd/passport/common/LoginConstant.java
@@ -27,7 +27,7 @@ public class LoginConstant {
     /**
      * 某IP连续登陆失败多少次需要用户在登陆时输入验证码 >=
      */
-    public static final int LOGIN_FAILED_NEED_CAPTCHA_IP_LIMIT_COUNT = 100;
+    public static final int LOGIN_FAILED_NEED_CAPTCHA_IP_LIMIT_COUNT = 50;
 
     /**
      * 某IP连续登陆失败多少次需要用户在登陆时输入验证码 >=
@@ -44,7 +44,7 @@ public class LoginConstant {
     /**
      * 一小时内IP登陆成功多少次之后，用户不能再登陆
      */
-    public static final int LOGIN_IP_SUCCESS_EXCEED_MAX_LIMIT_COUNT = 1000;
+    public static final int LOGIN_IP_SUCCESS_EXCEED_MAX_LIMIT_COUNT = 500;
 
     /**
      * 内部接口一小时内某IP验证失败多少次之后，用户不能再登陆
