bef5ee310b2295ce8ac29d311d3845bb7e30d0c4
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2014-01-10 14:19:33 +0800

    搜狗隐藏授权+sg cookie

--- passport-main-manager/src/test/java/com/sogou/upd/passport/manager/account/SGLoginApiManagerTest.java
+++ passport-main-manager/src/test/java/com/sogou/upd/passport/manager/account/SGLoginApiManagerTest.java
@@ -1,13 +1,21 @@
 package com.sogou.upd.passport.manager.account;
 
 import com.sogou.upd.passport.common.math.Coder;
+import com.sogou.upd.passport.common.math.RSA;
+import com.sogou.upd.passport.common.model.httpclient.RequestModel;
 import com.sogou.upd.passport.common.result.Result;
+import com.sogou.upd.passport.common.utils.SGHttpClient;
 import com.sogou.upd.passport.manager.api.account.LoginApiManager;
 import com.sogou.upd.passport.manager.api.account.form.AuthUserApiParams;
+import com.sogou.upd.passport.manager.api.account.form.CookieApiParams;
+import com.sogou.upd.passport.manager.api.account.impl.SGLoginApiManagerImpl;
+import junit.framework.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA. User: chenjiameng Date: 13-5-15 Time: 下午4:31 To change this template use
@@ -18,16 +26,8 @@ public class SGLoginApiManagerTest extends AbstractJUnit4SpringContextTests {
     @Autowired
     private LoginApiManager sgLoginApiManager;
 
-    private static final String PASSPORT_ID = "13552848876@sohu.com";
-    private static final String NEW_MOBILE = "13800000000";
-    private static final String EMAIL = "Binding123@163.com";
-    private static final String NEW_EMAIL = "hujunfei1986@126.com";
-    private static final String QUESTION = "Secure question";
-    private static final String NEW_QUESTION = "New secure question";
-    private static final String ANSWER = "Secure answer";
-    private static final String NEW_ANSWER = "New secure answer";
-
     private static final int clientId = 1100;
+
     @Test
     public void testAuthUser() {
         try {
@@ -37,10 +37,48 @@ public class SGLoginApiManagerTest extends AbstractJUnit4SpringContextTests {
             authUserParameters.setPassword(Coder.encryptMD5("123456"));
             authUserParameters.setUsertype(1);
             Result result = sgLoginApiManager.webAuthUser(authUserParameters);
-            System.out.println("testAuthUser:"+result);
+            System.out.println("testAuthUser:" + result);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void testGetCookieInfo() {
+        String userid = "shipengzhi1986@sogou.com";
+        int client_id = 1120;
+        String uniqname = "跳刀的兔子";
+        String refnick = "跳刀的兔子";
+        // userid, client_id, ru, ip, uniqname, refnick
+        CookieApiParams cookieApiParams = new CookieApiParams(userid, client_id, "", "", uniqname, refnick);
+        Result result = sgLoginApiManager.getCookieInfo(cookieApiParams);
+        System.out.println("sginf: " + result.getModels().get("sginf"));
+        System.out.println("sgrdig: " + result.getModels().get("sgrdig"));
+    }
+
+
+    @Test
+    public void testVerifyCookie() {
+        String passportId = "CFBF0F59AE1029AF7C2F9F1CD4827F96@qq.sohu.com";
+        int client_id = 1120;
+        CookieApiParams cookieApiParams = new CookieApiParams(passportId, client_id, "", "", "", "");
+        Result result = sgLoginApiManager.getCookieInfo(cookieApiParams);
+        if (result.isSuccess()) {
+            Map map = result.getModels();
+            String sginf = (String) map.get("sginf");
+            String sgrdig = (String) map.get("sgrdig");
+            RequestModel requestModel = new RequestModel("http://10.11.195.95/");
+            requestModel.addHeader("Cookie", "sginf=" + sginf + ";" + "sgrdig=" + sgrdig);
+            try {
+                String response = SGHttpClient.executeStr(requestModel);
+                Assert.assertEquals(passportId, response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Assert.assertTrue(false);
+        }
+    }
+
 
 }
