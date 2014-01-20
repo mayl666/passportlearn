b4ed2f684e96954b1ce856e660a61793714c0935
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2014-01-20 15:29:03 +0800

    大小写在参数类层更改

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/account/form/CheckUserNameExistParameters.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/form/CheckUserNameExistParameters.java
@@ -1,32 +1,14 @@
 package com.sogou.upd.passport.web.account.form;
 
-import com.google.common.base.Strings;
-import com.sogou.upd.passport.common.utils.PhoneUtil;
-import com.sogou.upd.passport.common.validation.constraints.UserName;
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.NotBlank;
 
-import javax.validation.constraints.AssertTrue;
+import com.sogou.upd.passport.manager.form.UsernameParams;
 
 /**
  * User: mayan
  * Date: 13-4-15 Time: 下午5:15
  */
-public class CheckUserNameExistParameters {
-    @Length(min = 1, max = 100, message = "用户名错误！")
-    @NotBlank(message = "用户名不允许为空!")
-    private String username;
-
+public class CheckUserNameExistParameters extends UsernameParams {
     private String client_id;
-
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     public String getClient_id() {
         return client_id;
