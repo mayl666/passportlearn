0163b498f6376c9d52a9969b52c5642df917c75f
yinyong <yinyong@sogou-inc.com>
2014-02-13 18:16:00 +0800

    更新pcaccount

--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -7,6 +7,6 @@
     }else{
         passhref = 'http://s.account.sogoucdn.com/u';
     }
-    scr.src= passhref + '/api/sogou.js?t=2014021320';
+    scr.src= passhref + '/api/sogou.js?t=2014021350';
     document.body.appendChild(scr);
 })();
