a4a51001590397ff2dc12cf3f2efa6d397ff6228
mayan <mayan@sogou-inc.com>
2014-02-28 14:15:38 +0800

    修改获取昵称bug

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/account/impl/OAuth2ResourceManagerImpl.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/account/impl/OAuth2ResourceManagerImpl.java
@@ -290,9 +290,7 @@ public class OAuth2ResourceManagerImpl implements OAuth2ResourceManager {
             AccountDomainEnum domain = AccountDomainEnum.getAccountDomain(passportId);
             if (domain == AccountDomainEnum.THIRD) {
                 Account account=commonManager.queryAccountByPassportId(passportId);
-                if(account!=null){
-                    uniqname=account.getUniqname();
-                }else {
+                if(account!=null && Strings.isNullOrEmpty(account.getUniqname())){
                     ConnectToken connectToken=getConnectToken(passportId,clientId);
                     if(connectToken!=null){
                         uniqname=connectToken.getConnectUniqname();
