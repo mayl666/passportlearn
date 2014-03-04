63e5bf768f055461e40d702f2c98d68ad38a3343
liuling <liuling@sogou-inc.com>
2014-03-04 16:26:45 +0800

    第三方原始及非原始信息缓存修改，非原始信息缓存3个月，原始信息缓存1天；其中缓存涉及到的接口还有第三方登录成功后的回调接口，主要是缓存的设置

--- passport-main-service/src/test/java/com/sogou/upd/passport/dao/SGStringHashRouterTest.java
+++ passport-main-service/src/test/java/com/sogou/upd/passport/dao/SGStringHashRouterTest.java
@@ -28,8 +28,8 @@ public class SGStringHashRouterTest extends TestCase {
      * 测试sg-string-hash计算是否正确
      */
     public void testRoute() {
-        router = new SGStringHashRouter("id", "account_{0}", 32);
-        String id = "A9DFD6E43441094F4CE5912F5F86009E@qq.sohu.com";
+        router = new SGStringHashRouter("id", "connect_token_{0}", 3);
+        String id = "CFF81AB013A94663D83FEC36AC117933@qq.sohu.com";
         String name = router.doRoute(id);
         System.out.println(name);
 //        Assert.assertEquals(name, "account_18");
