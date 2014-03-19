a9caec0793abdf096fc38b5d93409da464440ae6
yinyong <yinyong@sogou-inc.com>
2014-03-19 15:17:49 +0800

    修正了注册不跳转的bug

--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -7,6 +7,6 @@
     }else{
         passhref = 'http://s.account.sogoucdn.com/u';
     }
-    scr.src= passhref + '/api/sogou.js?t=2014031838';
+    scr.src= passhref + '/api/sogou.js?t=2014031937';
     document.body.appendChild(scr);
 })();
