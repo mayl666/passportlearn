3627f8e0e184b0fed1636e7ee9a7c1de4c7fba52
liuling <liuling@sogou-inc.com>
2014-03-20 15:34:52 +0800

    非第三方账号迁移注册代码修改:单点登录，第一次获取第三方用户信息时，取到第三方信息后写搜狗库

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/account/impl/OAuth2ResourceManagerImpl.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/account/impl/OAuth2ResourceManagerImpl.java
@@ -355,6 +355,12 @@ public class OAuth2ResourceManagerImpl implements OAuth2ResourceManager {
                                 tiny_avatar = (String) getPhotoResult.getModels().get("img_30");
                             }
                         }
+                    } else {
+                        //获取不同尺寸头像
+                        Result getPhotoResult = photoUtils.obtainPhoto(avatarurl, "30,50,180");
+                        large_avatar = (String) getPhotoResult.getModels().get("img_180");
+                        mid_avatar = (String) getPhotoResult.getModels().get("img_50");
+                        tiny_avatar = (String) getPhotoResult.getModels().get("img_30");
                     }
                 }
             } else {
