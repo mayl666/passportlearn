b1ff388b4e2b7bcc3720eb77ca80de0088aef552
yinyong <yinyong@sogou-inc.com>
2014-03-20 20:49:42 +0800

    邮箱激活失败

--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -7,6 +7,6 @@
     }else{
         passhref = 'http://s.account.sogoucdn.com/u';
     }
-    scr.src= passhref + '/api/sogou.js?t=2014032041';
+    scr.src= passhref + '/api/sogou.js?t=2014032032';
     document.body.appendChild(scr);
 })();
