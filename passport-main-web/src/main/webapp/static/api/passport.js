d7e0b42ad7543ea29d3e7a9d747ee9fcda383dfb
yinyong <yinyong@sogou-inc.com>
2014-03-21 16:15:21 +0800

    增加重发按钮

--- passport-main-web/src/main/webapp/static/api/passport.js
+++ passport-main-web/src/main/webapp/static/api/passport.js
@@ -5,5 +5,5 @@
     }else{
         passhref = 'http://s.account.sogoucdn.com/u';
     }
-    document.write('<script type="text/javascript" src="'+ passhref +'/api/sogou.js?t=2014032024"></script>');
+    document.write('<script type="text/javascript" src="'+ passhref +'/api/sogou.js?t=2014032109"></script>');
 })();
