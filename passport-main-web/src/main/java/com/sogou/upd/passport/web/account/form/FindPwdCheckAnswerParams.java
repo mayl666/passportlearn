5eb22e1af1ace008e3d1915c493836355aac095d
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2014-03-23 21:38:00 +0800

    找回密码参数类

---
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/form/FindPwdCheckAnswerParams.java
@@ -0,0 +1,11 @@
+package com.sogou.upd.passport.web.account.form;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenjiameng
+ * Date: 14-3-23
+ * Time: 下午9:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class FindPwdCheckAnswerParams {
+}
