2e2aa138f3f53b5067d63a3c2b32266f1983a12d
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2014-03-23 21:41:50 +0800

    找回密码参数类

---
+++ passport-main-common/src/main/java/com/sogou/upd/passport/common/validation/constraints/FindPwdTypeValidator.java
@@ -0,0 +1,11 @@
+package com.sogou.upd.passport.common.validation.constraints;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chenjiameng
+ * Date: 14-3-23
+ * Time: 下午9:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class FindPwdTypeValidator {
+}
