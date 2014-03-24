595fe95310496968aa9a11d9810b1c40cb109260
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2014-03-24 13:30:13 +0800

    setppcookie设定有效期

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/form/PPCookieParams.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/form/PPCookieParams.java
@@ -28,7 +28,9 @@ public class PPCookieParams {
     @NotBlank(message = "lastdomain不允许为空")
     private String lastdomain;
 
-    private long s=0; //时间戳，单位毫秒
+    private long s = 0; //时间戳，单位毫秒
+
+    private String livetime = "0"; //cookie有效期  1为2周
 
     @NotBlank(message = "ru不允许为空")
     @Ru
@@ -104,5 +106,13 @@ public class PPCookieParams {
 
     public void setCode(String code) {
         this.code = code;
+    }
+
+    public String getLivetime() {
+        return livetime;
+    }
+
+    public void setLivetime(String livetime) {
+        this.livetime = livetime;
     }
 }
