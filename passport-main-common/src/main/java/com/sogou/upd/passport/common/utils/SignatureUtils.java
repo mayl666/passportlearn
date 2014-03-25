0561a4589b715e18bb8b610e8617a65e59febecb
shipengzhi <shipengzhi@sogou-inc.com>
2014-03-25 14:50:02 +0800

    删除无用方法

--- passport-main-common/src/main/java/com/sogou/upd/passport/common/utils/SignatureUtils.java
+++ passport-main-common/src/main/java/com/sogou/upd/passport/common/utils/SignatureUtils.java
@@ -2,8 +2,6 @@ package com.sogou.upd.passport.common.utils;
 
 import com.sogou.upd.passport.common.math.Coder;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.Set;
 import java.util.TreeMap;
 
@@ -20,23 +18,10 @@ public class SignatureUtils {
         for (String key : sortKeys) {
             paramsBase.append(key).append("=").append(params.get(key)).append("&");
         }
-        String enParams=paramsBase.toString();
+        String enParams = paramsBase.toString();
 
         String baseStr = enParams + secret;
         return Coder.encryptMD5(baseStr);
     }
 
-    /**
-     * 将参数URLEncode为UTF-8
-     *
-     * @param params
-     * @return
-     * @throws java.io.UnsupportedEncodingException
-     */
-    public static String urlEncodeUTF8(String params) throws UnsupportedEncodingException {
-        String en = URLEncoder.encode(params, "UTF8");
-        en = en.replace("+", "%20");
-        en = en.replace("*", "%2A");
-        return en;
-    }
 }
