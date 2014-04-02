1342440200c39baeb8a57716980a0343bc9d9b40
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2014-04-02 12:10:39 +0800

    mergre from master

--- passport-main-common/src/main/java/com/sogou/upd/passport/common/utils/DBShardRedisUtils.java
+++ passport-main-common/src/main/java/com/sogou/upd/passport/common/utils/DBShardRedisUtils.java
@@ -277,6 +277,7 @@ public class DBShardRedisUtils {
                 T object = null;
                 if (!Strings.isNullOrEmpty(cacheStr)) {
                     object = (T) jsonMapper.readValue(cacheStr, returnClass);
+                    ;
                 }
                 return object;
             }
