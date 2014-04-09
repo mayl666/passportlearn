9eb5acf805d2370f662e25737246f12585d74be2
yinyong <yinyong@sogou-inc.com>
2014-04-09 20:05:19 +0800

    修正浏览器注册框提示信息

--- passport-main-web/src/main/webapp/static/api/passport.js
+++ passport-main-web/src/main/webapp/static/api/passport.js
@@ -5,5 +5,5 @@
     }else{
         passhref = 'http://s.account.sogoucdn.com/u';
     }
-    document.write('<script type="text/javascript" src="'+ passhref +'/api/sogou.js?t=2014040904"></script>');
+    document.write('<script type="text/javascript" src="'+ passhref +'/api/sogou.js?t=2014040908"></script>');
 })();
