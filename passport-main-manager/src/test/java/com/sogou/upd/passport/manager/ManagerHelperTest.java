5d467611a1913a0d35b4f4851b2b2f58d7198eb7
liuling <liuling@sogou-inc.com>
2014-04-11 12:47:12 +0800

    用户信息测试用例补全

--- passport-main-manager/src/test/java/com/sogou/upd/passport/manager/ManagerHelperTest.java
+++ passport-main-manager/src/test/java/com/sogou/upd/passport/manager/ManagerHelperTest.java
@@ -25,7 +25,7 @@ public class ManagerHelperTest extends TestCase {
         long ct = System.currentTimeMillis();
         System.out.println("ct:" + ct);
 //        String ct =  "1381915491000";
-        String code = ManagerHelper.generatorCodeGBK("tinkameabc010", clientId,serverSecret, ct);
+        String code = ManagerHelper.generatorCodeGBK("13581695053", clientId,serverSecret, ct);
         System.out.println("code:" + code);
 
         /*try {
