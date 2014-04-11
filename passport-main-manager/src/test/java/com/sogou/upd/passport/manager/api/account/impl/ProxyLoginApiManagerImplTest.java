9a9981c304ab7d11c2931391143f423d22ea23cc
chenjiameng <chenjiameng@CHENJIAMENG.sogou-inc.com>
2014-04-11 21:51:08 +0800

    发送手机验证码key不能加clientid

--- passport-main-manager/src/test/java/com/sogou/upd/passport/manager/api/account/impl/ProxyLoginApiManagerImplTest.java
+++ passport-main-manager/src/test/java/com/sogou/upd/passport/manager/api/account/impl/ProxyLoginApiManagerImplTest.java
@@ -25,20 +25,20 @@ public class ProxyLoginApiManagerImplTest extends BaseTest {
     @Test
     public void testAuthUser() {
         try {
-//            AuthUserApiParams authUserParameters = new AuthUserApiParams();
-//            authUserParameters.setUserid("tinkame731@17173.com");
-//            authUserParameters.setClient_id(1120);
+            AuthUserApiParams authUserParameters = new AuthUserApiParams();
+            authUserParameters.setUserid("tinkame731@17173.com");
+            authUserParameters.setClient_id(1120);
 //            authUserParameters.setIp("10.129.192.193");
-//            authUserParameters.setPassword(Coder.encryptMD5("cjm136210"));
+            authUserParameters.setPassword(Coder.encryptMD5("cjm136210"));
 //            Result result = proxyLoginApiManager.webAuthUser(authUserParameters);
 
 //
-            AuthUserApiParams authUserParameters = new AuthUserApiParams();
-            authUserParameters.setUserid("techchina@17173.com");
-            authUserParameters.setClient_id(1120);
-            authUserParameters.setIp("10.129.192.193");
-//            authUserParameters.setPassword("e355b0428c262b7c883a724a002bf8b8");
-            authUserParameters.setPassword(Coder.encryptMD5("cd98203213"));
+//            AuthUserApiParams authUserParameters = new AuthUserApiParams();
+//            authUserParameters.setUserid("techchina@17173.com");
+//            authUserParameters.setClient_id(1120);
+//            authUserParameters.setIp("10.129.192.193");
+////            authUserParameters.setPassword("e355b0428c262b7c883a724a002bf8b8");
+//            authUserParameters.setPassword(Coder.encryptMD5("cd98203213"));
             Result result = proxyLoginApiManager.webAuthUser(authUserParameters);
             System.out.println("resultStr:"+result);
         } catch (Exception e) {
