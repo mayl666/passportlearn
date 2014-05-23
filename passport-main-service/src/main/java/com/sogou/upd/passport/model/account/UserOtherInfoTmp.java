9ac94a021ec6f73aade8a9f60f6e26e1720be920
shipengzhi <shipengzhi@sogou-inc.com>
2014-05-23 10:59:21 +0800

    增加搜狐全量数据临时表的DAO

---
+++ passport-main-service/src/main/java/com/sogou/upd/passport/model/account/UserOtherInfoTmp.java
@@ -0,0 +1,93 @@
+package com.sogou.upd.passport.model.account;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shipengzhi
+ * Date: 14-5-23
+ * Time: 上午12:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserOtherInfoTmp {
+
+    private String userid;
+    private String personalid;
+    private String mobile;
+    private String mobileflag;
+    private String email;
+    private String emailflag;
+    private String province;
+    private String uniqname;
+    private String city;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPersonalid() {
+        return personalid;
+    }
+
+    public void setPersonalid(String personalid) {
+        this.personalid = personalid;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getMobileflag() {
+        return mobileflag;
+    }
+
+    public void setMobileflag(String mobileflag) {
+        this.mobileflag = mobileflag;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmailflag() {
+        return emailflag;
+    }
+
+    public void setEmailflag(String emailflag) {
+        this.emailflag = emailflag;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getUniqname() {
+        return uniqname;
+    }
+
+    public void setUniqname(String uniqname) {
+        this.uniqname = uniqname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
