125b91b0a1ddf910a8bd6f22f2f07fda4033f5b0
liuling <liuling@sogou-inc.com>
2014-05-27 16:58:43 +0800

    桌面端token缓存有效期修改为1个月，解决缓存容量增长问题

--- passport-main-common/src/main/java/com/sogou/upd/passport/common/DateAndNumTimesConstant.java
+++ passport-main-common/src/main/java/com/sogou/upd/passport/common/DateAndNumTimesConstant.java
@@ -8,9 +8,10 @@ public class DateAndNumTimesConstant {
 
     public static final long TIME_ONEDAY = 24 * 60 * 60; // 时间 一天 1440分钟 ,单位s
     public static final long TIME_FIVE_MINITUES = 5 * 60; // 时间 一天 1440分钟 ,单位s
-    public static final long TWO_WEEKS =14 * 24 * 60 * 60; // 时间 一天 1440分钟 ,单位s
-    public static final long SIX_MONTH =6 * 30 * 24 * 60 * 60; // 时间 6个月 ,单位s
-    public static final long THREE_MONTH =3 * 30 * 24 * 60 * 60; // 时间 6个月 ,单位s
+    public static final long TWO_WEEKS = 14 * 24 * 60 * 60; // 时间 一天 1440分钟 ,单位s
+    public static final long SIX_MONTH = 6 * 30 * 24 * 60 * 60; // 时间 6个月 ,单位s
+    public static final long THREE_MONTH = 3 * 30 * 24 * 60 * 60; // 时间 6个月 ,单位s
+    public static final long ONE_MONTH = 1 * 30 * 24 * 60 * 60; // 时间 1个月 ,单位s
     public static final long TIME_TWODAY = 2 * 24 * 60 * 60; // 时间 两天 2880分钟 ,单位s
     public static final long IP_LIMITED = 20; // ip一天限制次数
     public static final int RESETPWD_NUM = 10; // 密码修改一天限制次数
@@ -21,13 +22,11 @@ public class DateAndNumTimesConstant {
     public static final int BIND_LIMIT = 10;   // 每日绑定限制次数
     public static final int ACTIONRECORD_NUM = 10; // 动作记录条数
 
-    public static final int ONE_HOUR_INSECONDS = 60*60*1; // 1小时,单位,s
-    public static final int ONE_MONTH_INSECONDS = 60*60*24*30; // 10天,单位,s
-    public static final int ONE_DAY_INSECONDS = 60*60*24; // 1天,单位,s
+    public static final int ONE_HOUR_INSECONDS = 60 * 60 * 1; // 1小时,单位,s
+    public static final int ONE_MONTH_INSECONDS = 60 * 60 * 24 * 30; // 10天,单位,s
+    public static final int ONE_DAY_INSECONDS = 60 * 60 * 24; // 1天,单位,s
 
     public static final long TIME_FIVEMINUTES = 5 * 60; // 时间 5分钟,单位s
-
-
 
 
 }
