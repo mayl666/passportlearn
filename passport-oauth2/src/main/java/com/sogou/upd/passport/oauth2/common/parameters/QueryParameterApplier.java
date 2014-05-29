508192f388ce1f490d8ceeb2e9695a77d3081689
liuling <liuling@sogou-inc.com>
2014-05-29 16:34:27 +0800

    develop分支测试scope参数不编码的情况;

--- passport-oauth2/src/main/java/com/sogou/upd/passport/oauth2/common/parameters/QueryParameterApplier.java
+++ passport-oauth2/src/main/java/com/sogou/upd/passport/oauth2/common/parameters/QueryParameterApplier.java
@@ -3,6 +3,7 @@ package com.sogou.upd.passport.oauth2.common.parameters;
 import com.google.common.base.Strings;
 import com.sogou.upd.passport.common.CommonConstant;
 import com.sogou.upd.passport.oauth2.authzserver.response.OAuthMessage;
+import com.sogou.upd.passport.oauth2.common.OAuth;
 import com.sogou.upd.passport.oauth2.common.utils.OAuthUtils;
 
 import java.util.Map;
@@ -28,15 +29,24 @@ public class QueryParameterApplier implements OAuthParametersApplier {
     public static String applyOAuthParametersString(String messageUrl, Map<String, Object> params) {
         boolean isContainsQuery = messageUrl.contains("?");
         StringBuilder url = new StringBuilder(messageUrl);
-        //scope参数不编码，QQ和RENREN,scope目前不为空，BAIDU和SINA的scope值为空
+
+        //scope参数不编码，QQ和RENREN,scope目前不为空，BAIDU和SINA的scope值为空,编码时暂时移除
         Object scope = null;
-        if (params.containsKey("scope") && params.get("scope") != null) {
-            scope = params.get("scope");
-            params.remove("scope");
+        if (params.containsKey(OAuth.OAUTH_SCOPE) && params.get(OAuth.OAUTH_SCOPE) != null) {
+            scope = params.get(OAuth.OAUTH_SCOPE);
+            params.remove(OAuth.OAUTH_SCOPE);
         }
+
         StringBuilder query = new StringBuilder(OAuthUtils.format(params.entrySet(),
                 CommonConstant.DEFAULT_CONTENT_CHARSET));
-        params.put("scope", scope);
+
+        //编码后将scope添加到query中
+        if (query.length() > 0) {
+            query.append(OAuthUtils.PARAMETER_SEPARATOR);
+        }
+        query.append(OAuth.OAUTH_SCOPE);
+        query.append(OAuthUtils.NAME_VALUE_SEPARATOR);
+        query.append(scope);
         if (!Strings.isNullOrEmpty(query.toString())) {
             if (isContainsQuery) {
                 url.append("&").append(query);
