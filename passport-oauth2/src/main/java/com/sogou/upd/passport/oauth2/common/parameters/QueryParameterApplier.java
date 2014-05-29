9d19578448786900ef6620a75488886900a501cb
liuling <liuling@sogou-inc.com>
2014-05-29 19:16:30 +0800

    develop分支测试scope参数编码时是否可以正确解析scope

--- passport-oauth2/src/main/java/com/sogou/upd/passport/oauth2/common/parameters/QueryParameterApplier.java
+++ passport-oauth2/src/main/java/com/sogou/upd/passport/oauth2/common/parameters/QueryParameterApplier.java
@@ -3,7 +3,6 @@ package com.sogou.upd.passport.oauth2.common.parameters;
 import com.google.common.base.Strings;
 import com.sogou.upd.passport.common.CommonConstant;
 import com.sogou.upd.passport.oauth2.authzserver.response.OAuthMessage;
-import com.sogou.upd.passport.oauth2.common.OAuth;
 import com.sogou.upd.passport.oauth2.common.utils.OAuthUtils;
 
 import java.util.Map;
@@ -31,22 +30,22 @@ public class QueryParameterApplier implements OAuthParametersApplier {
         StringBuilder url = new StringBuilder(messageUrl);
 
         //scope参数不编码，QQ和RENREN,scope目前不为空，BAIDU和SINA的scope值为空,编码时暂时移除
-        Object scope = null;
-        if (params.containsKey(OAuth.OAUTH_SCOPE) && params.get(OAuth.OAUTH_SCOPE) != null) {
-            scope = params.get(OAuth.OAUTH_SCOPE);
-            params.remove(OAuth.OAUTH_SCOPE);
-        }
+//        Object scope = null;
+//        if (params.containsKey(OAuth.OAUTH_SCOPE) && params.get(OAuth.OAUTH_SCOPE) != null) {
+//            scope = params.get(OAuth.OAUTH_SCOPE);
+//            params.remove(OAuth.OAUTH_SCOPE);
+//        }
 
         StringBuilder query = new StringBuilder(OAuthUtils.format(params.entrySet(),
                 CommonConstant.DEFAULT_CONTENT_CHARSET));
 
         //编码后将scope添加到query中
-        if (query.length() > 0) {
-            query.append(OAuthUtils.PARAMETER_SEPARATOR);
-        }
-        query.append(OAuth.OAUTH_SCOPE);
-        query.append(OAuthUtils.NAME_VALUE_SEPARATOR);
-        query.append(scope);
+//        if (query.length() > 0) {
+//            query.append(OAuthUtils.PARAMETER_SEPARATOR);
+//        }
+//        query.append(OAuth.OAUTH_SCOPE);
+//        query.append(OAuthUtils.NAME_VALUE_SEPARATOR);
+//        query.append(scope);
         if (!Strings.isNullOrEmpty(query.toString())) {
             if (isContainsQuery) {
                 url.append("&").append(query);
