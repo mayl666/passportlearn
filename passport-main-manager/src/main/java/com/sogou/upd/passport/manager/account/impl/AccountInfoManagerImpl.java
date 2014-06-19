428a6482e9f4b111b26c65eb364099e24b35bd72
admin <chengang@sogou-inc.com>
2014-06-19 17:22:54 +0800

    fix user info update nickname bug

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/account/impl/AccountInfoManagerImpl.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/account/impl/AccountInfoManagerImpl.java
@@ -539,12 +539,7 @@ public class AccountInfoManagerImpl implements AccountInfoManager {
             //从论坛获取昵称
             uniqname = pcAccountManager.getBrowserBbsUniqname(passportId);
             if (isValidUniqname(passportId, uniqname)) {
-                String checkExist = passportId;
-                try {
-                    checkExist = accountService.checkUniqName(uniqname);
-                } catch (Exception e) {
-                }
-                if (account != null && Strings.isNullOrEmpty(checkExist)) {
+                if (account != null) {
                     //更新用户昵称信息到account表
                     //从浏览器论坛获取昵称、更新到account以及u_p_m、先check u_p_m昵称唯一性
                     boolean updateFlag = accountService.updateUniqName(account, uniqname);
