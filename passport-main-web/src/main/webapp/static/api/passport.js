42a42fc3125b9bbc75778708f09360ceeb199803
yinyong <yinyong@sogou-inc.com>
2014-06-30 20:02:05 +0800

    移除注册md5

--- passport-main-web/src/main/webapp/static/api/passport.js
+++ passport-main-web/src/main/webapp/static/api/passport.js
@@ -8,5 +8,5 @@
     if (undefined !== window.__sogoujsStartLoading) {
         window.__sogoujsStartLoading = +new Date;
     }
-    document.write('<script type="text/javascript" src="'+ passhref +'/api/sogou.js?t=2014062657"></script>');
+    document.write('<script type="text/javascript" src="'+ passhref +'/api/sogou.js?t=2014063050"></script>');
 })();
