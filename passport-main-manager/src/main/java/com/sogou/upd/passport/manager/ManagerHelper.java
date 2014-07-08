4f68819b6235387c155858ddabb2e32d2b69e0b4
liuling <liuling@sogou-inc.com>
2014-07-08 18:15:14 +0800

    Merge branch 'master' into both_write
    Conflicts:
    	passport-main-manager/src/main/java/com/sogou/upd/passport/manager/account/impl/CommonManagerImpl.java
    	passport-main-manager/src/test/resources/SQL/import_data_account.sql
    	passport-main-service/src/test/resources/sql/import_data_account.sql

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/ManagerHelper.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/ManagerHelper.java
@@ -62,23 +62,23 @@ public class ManagerHelper {
     }
 
     /**
-     * 是否需要只读SH库。
-     *
-     * @return
-     */
-    public static boolean writeSohuSwitcher() {
-//        return true;   //todo 若非上线后出故障，回滚至SOHU代码，打开此开关，即为回滚
-        return false; //todo 正常线上都应该恒为false
-    }
-
-    /**
-     * 是否需要只读SG库。
+     * 是否需要只读SG库。当isBothReadApi方法返回false时：此方法返回true表示只读SG库；返回false表示只读SH线上，相当于回滚操作
      *
      * @return
      */
     public static boolean readSohuSwitcher() {
 //        return true;   //todo 若非上线后出故障，回滚至SOHU代码，打开此开关，即为回滚
         return false; //todo 正常线上都应该恒为false
+    }
+
+    /**
+     * 是否需要只写SH。搜狗账号写分离时返回为false
+     *
+     * @return
+     */
+    public static boolean writeSohuSwitcher() {
+//        return true;
+        return false;
     }
 
 
