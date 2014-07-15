eed66aa1b08bc63dbc434abdcba931d3d6bc7712
shipengzhi <shipengzhi@sogou-inc.com>
2014-07-16 03:25:30 +0800

    外域、手机账号绑定邮箱ru没有做urlencode

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/api/account/impl/ProxyBindApiManagerImpl.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/api/account/impl/ProxyBindApiManagerImpl.java
@@ -1,25 +1,16 @@
 package com.sogou.upd.passport.manager.api.account.impl;
 
-import com.sogou.upd.passport.common.CacheConstant;
-import com.sogou.upd.passport.common.lang.StringUtil;
 import com.sogou.upd.passport.common.model.httpclient.RequestModelXml;
 import com.sogou.upd.passport.common.result.Result;
-import com.sogou.upd.passport.common.utils.ErrorUtil;
-import com.sogou.upd.passport.common.utils.RedisUtils;
-import com.sogou.upd.passport.common.utils.SMSUtil;
-import com.sogou.upd.passport.exception.ServiceException;
 import com.sogou.upd.passport.manager.api.BaseProxyManager;
 import com.sogou.upd.passport.manager.api.SHPPUrlConstant;
 import com.sogou.upd.passport.manager.api.account.BindApiManager;
 import com.sogou.upd.passport.manager.api.account.form.BaseMoblieApiParams;
 import com.sogou.upd.passport.manager.api.account.form.BindEmailApiParams;
-import com.sogou.upd.passport.manager.api.account.form.BindMobileApiParams;
-import com.sogou.upd.passport.manager.api.account.form.SendCaptchaApiParams;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * User: ligang201716@sogou-inc.com
@@ -32,6 +23,12 @@ public class ProxyBindApiManagerImpl extends BaseProxyManager implements BindApi
     @Override
     public Result bindEmail(BindEmailApiParams bindEmailApiParams) {
         RequestModelXml requestModelXml = new RequestModelXml(SHPPUrlConstant.BIND_EMAIL, SHPPUrlConstant.DEFAULT_REQUEST_ROOTNODE);
+        try {
+            String ru = bindEmailApiParams.getRu();
+            ru = URLEncoder.encode(ru, "UTF-8");
+            bindEmailApiParams.setRu(ru);
+        } catch (UnsupportedEncodingException e) {
+        }
         bindEmailApiParams.setPwdtype(1);
         requestModelXml.addParams(bindEmailApiParams);
         requestModelXml.addHeader("Accept-Language", "zh-CN");
@@ -46,10 +43,10 @@ public class ProxyBindApiManagerImpl extends BaseProxyManager implements BindApi
     }
 
     @Override
-    public Result bindMobile(String passportId,String newMobile){
+    public Result bindMobile(String passportId, String newMobile) {
         RequestModelXml requestModelXml = new RequestModelXml(SHPPUrlConstant.BING_MOBILE, SHPPUrlConstant.DEFAULT_REQUEST_ROOTNODE);
-        requestModelXml.addParam("mobile",newMobile);
-        requestModelXml.addParam("userid",passportId);
+        requestModelXml.addParam("mobile", newMobile);
+        requestModelXml.addParam("userid", passportId);
         return executeResult(requestModelXml);
     }
 
@@ -60,14 +57,15 @@ public class ProxyBindApiManagerImpl extends BaseProxyManager implements BindApi
 
     /**
      * 解除手机号绑定
+     *
      * @param mobile
      * @return
      */
     @Override
-    public Result unBindMobile(String mobile){
+    public Result unBindMobile(String mobile) {
         RequestModelXml requestModelXml = new RequestModelXml(SHPPUrlConstant.UNBING_MOBILE, SHPPUrlConstant.DEFAULT_REQUEST_ROOTNODE);
-        requestModelXml.addParam("mobile",mobile);
-        return executeResult(requestModelXml,mobile);
+        requestModelXml.addParam("mobile", mobile);
+        return executeResult(requestModelXml, mobile);
     }
 
 
