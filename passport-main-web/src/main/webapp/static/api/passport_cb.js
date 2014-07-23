1ca0e6aae57cfad2b4303dfbfdbf302bbbba314b
zhengxin <demixcn@gmail.com>
2014-07-23 16:35:43 +0800

    bug fix for mobile findpwd

--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -7,7 +7,7 @@
     }else{
         passhref = 'http://s.account.sogoucdn.com/u';
     }
-    scr.src= passhref + '/api/sogou.js?t=2014072300';
+    scr.src= passhref + '/api/sogou.js?t=2014072303';
     if(undefined !==window.__sogoujsStartLoading){
         window.__sogoujsStartLoading = + new Date;
     }
