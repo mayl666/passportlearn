bc53e22e52b493ebc37fb38f6724bc4a6dd7a33e
liuling <liuling@sogou-inc.com>
2014-07-28 16:01:38 +0800

    将注册的checkuser的返回结果转成登录的

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/account/impl/LoginManagerImpl.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/account/impl/LoginManagerImpl.java
@@ -68,7 +68,7 @@ public class LoginManagerImpl implements LoginManager {
             CheckUserApiParams checkUserApiParams = buildProxyApiParams(username, clientId);
             String passportId = commonManager.getPassportIdByUsername(username);
             if (Strings.isNullOrEmpty(passportId)) {
-                result.setCode(ErrorUtil.ERR_CODE_ACCOUNT_PHONE_NOBIND);
+                result.setCode(ErrorUtil.ERR_CODE_ACCOUNT_NOTHASACCOUNT);
                 return result;
             }
             if (AccountDomainEnum.SOHU.equals(AccountDomainEnum.getAccountDomain(passportId))) {
@@ -76,11 +76,24 @@ public class LoginManagerImpl implements LoginManager {
             } else {
                 result = sgRegisterApiManager.checkUser(checkUserApiParams);
             }
+            buildLoginResult(result);
         } catch (ServiceException e) {
             logger.error("Check account is exists Exception, username:" + username, e);
             throw new ServiceException(e);
         }
         return result;
+    }
+
+    private void buildLoginResult(Result result) {
+        //注册的checkuser返回结果为false有可能是账号已存在，当提示账号已存在或已注册时，需要转成登录的结果
+        if (!result.isSuccess() && (ErrorUtil.ERR_CODE_USER_ID_EXIST.equals(result.getCode()) || ErrorUtil.ERR_CODE_ACCOUNT_REGED.equals(result.getCode()))) {
+            result.setSuccess(true);
+        }
+        //注册checkuser返回结果为true，表示账号不存在，需要转成登录的结果
+        if (result.isSuccess()) {
+            result.setSuccess(false);
+            result.setCode(ErrorUtil.ERR_CODE_ACCOUNT_NOTHASACCOUNT);
+        }
     }
 
     private CheckUserApiParams buildProxyApiParams(String username, int clientId) {
