016a3d2f4aaafab4054a9d1a35415f69e5271020
liuling <liuling@sogou-inc.com>
2014-07-30 11:10:55 +0800

    中文乱码问题测试

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/account/action/wap/WapResetPwdAction.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/action/wap/WapResetPwdAction.java
@@ -35,6 +35,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * 移动端找回密码
@@ -388,7 +390,7 @@ public class WapResetPwdAction extends BaseController {
     }
 
     //验证完邮件跳转至页面提示重置密码页
-    private String buildSendRedirectUrl(Result result, WapCheckEmailParams params) {
+    private String buildSendRedirectUrl(Result result, WapCheckEmailParams params) throws UnsupportedEncodingException {
         String ru = Strings.isNullOrEmpty(params.getRu()) ? Coder.encodeUTF8(CommonConstant.DEFAULT_WAP_URL) : Coder.encodeUTF8(params.getRu());
         String client_id = Strings.isNullOrEmpty(params.getClient_id()) ? String.valueOf(CommonConstant.SGPP_DEFAULT_CLIENTID) : params.getClient_id();
         StringBuilder urlStr = new StringBuilder();
@@ -396,7 +398,7 @@ public class WapResetPwdAction extends BaseController {
         urlStr.append("username=" + params.getUsername());
         urlStr.append("&client_id=" + client_id);
         urlStr.append("&ru=" + ru);
-        urlStr.append("&message=" + result.getMessage());
+        urlStr.append("&message=" + URLEncoder.encode(result.getMessage(),"GBK"));
         urlStr.append("&skin=" + params.getSkin());
         urlStr.append("&v=" + params.getV());
         return urlStr.toString();
