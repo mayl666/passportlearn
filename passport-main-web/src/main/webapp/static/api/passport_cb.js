44b0a0b4b1109c987e8c4fe258149b3953c969d1
yinyong <yinyong@sogou-inc.com>
2014-08-01 10:53:13 +0800

    remove mobile num

--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -7,7 +7,7 @@
     }else{
         passhref = 'http://s.account.sogoucdn.com/u';
     }
-    scr.src= passhref + '/api/sogou.js?t=2014073116';
+    scr.src= passhref + '/api/sogou.js?t=2014080158';
     if(undefined !==window.__sogoujsStartLoading){
         window.__sogoujsStartLoading = + new Date;
     }
