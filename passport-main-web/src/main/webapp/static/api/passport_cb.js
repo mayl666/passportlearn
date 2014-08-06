1cd94c8692e4dfd5195e70451b1c995280dc6020
yinyong <yinyong@sogou-inc.com>
2014-08-06 16:33:52 +0800

    fixed bug

--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -7,7 +7,7 @@
     }else{
         passhref = 'http://s.account.sogoucdn.com/u';
     }
-    scr.src= passhref + '/api/sogou.js?t=2014080636';
+    scr.src= passhref + '/api/sogou.js?t=2014080637';
     if(undefined !==window.__sogoujsStartLoading){
         window.__sogoujsStartLoading = + new Date;
     }
