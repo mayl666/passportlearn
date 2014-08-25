1185bf33ee530b179fdc4fb76d553d1f4bcfcd42
shipengzhi <shipengzhi@sogou-inc.com>
2014-08-25 17:25:28 +0800

    删除无用的增量数据同步，缓存接口

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/api/CacheSyncUpdateManager.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/api/CacheSyncUpdateManager.java
@@ -10,8 +10,6 @@ import com.sogou.upd.passport.common.utils.ErrorUtil;
 import com.sogou.upd.passport.common.utils.RedisUtils;
 import com.sogou.upd.passport.common.utils.TokenRedisUtils;
 import com.sogou.upd.passport.exception.ServiceException;
-import com.sogou.upd.passport.service.account.AccountInfoService;
-import com.sogou.upd.passport.service.account.AccountService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,34 +30,11 @@ public class CacheSyncUpdateManager {
     private static Logger log = LoggerFactory.getLogger(CacheSyncUpdateManager.class);
 
     @Autowired
-    private AccountService accountService;
-    @Autowired
-    private AccountInfoService accountInfoService;
-    @Autowired
     private DBShardRedisUtils dbShardRedisUtils;
     @Autowired
     private RedisUtils redisUtils;
     @Autowired
     private TokenRedisUtils tokenRedisUtils;
-
-    public Result deleteTableCache(String passportId) {
-        Result result = new APIResultSupport(false);
-        try {
-            boolean isDelAccount = accountService.deleteAccountCacheByPassportId(passportId);
-            boolean isDelAccountInfo = accountInfoService.deleteAccountInfoCacheByPassportId(passportId);
-            if (isDelAccount && isDelAccountInfo) {
-//                    mobilePassportMappingService.deleteMobilePassportMappingCache(key);
-                result.setSuccess(true);
-                result.setMessage("passportId：" + passportId + " delete cache success!");
-            } else {
-                result.setCode(ErrorUtil.SYSTEM_UNKNOWN_EXCEPTION);
-            }
-        } catch (ServiceException e) {
-            log.error("passportId：" + passportId + " delete cache fail!", e);
-            result.setCode(ErrorUtil.SYSTEM_UNKNOWN_EXCEPTION);
-        }
-        return result;
-    }
 
     public Result readOperCache(String cacheKey, String cacheType, String cacheOper) {
         Result result = new APIResultSupport(false);
