124510fd2055ed9cfcf7781508be4b89fd0f63ae
liuling <liuling@sogou-inc.com>
2014-08-25 19:12:56 +0800

    微信登录的验证access_token是否有效的调用，直接继承自父类

--- passport-oauth2/src/main/java/com/sogou/upd/passport/oauth2/openresource/response/accesstoken/WeixinJSONVerifyAccessTokenResponse.java
+++ passport-oauth2/src/main/java/com/sogou/upd/passport/oauth2/openresource/response/accesstoken/WeixinJSONVerifyAccessTokenResponse.java
@@ -4,6 +4,7 @@ import com.sogou.upd.passport.common.utils.ErrorUtil;
 import com.sogou.upd.passport.common.utils.JacksonJsonMapperUtil;
 import com.sogou.upd.passport.oauth2.common.exception.OAuthProblemException;
 import com.sogou.upd.passport.oauth2.openresource.parameters.WeiXinOAuthError;
+import com.sogou.upd.passport.oauth2.openresource.response.OAuthClientResponse;
 import com.sogou.upd.passport.oauth2.openresource.validator.impl.WeiXinAPIValidator;
 import com.sogou.upd.passport.oauth2.openresource.vo.OAuthTokenVO;
 import com.sogou.upd.passport.oauth2.openresource.vo.WeixinOAuthTokenVO;
@@ -17,7 +18,7 @@ import java.util.Map;
  * Time: 下午3:58
  * To change this template use File | Settings | File Templates.
  */
-public class WeixinJSONVerifyAccessTokenResponse extends OAuthAccessTokenResponse {
+public class WeixinJSONVerifyAccessTokenResponse extends OAuthClientResponse {
 
     @Override
     public void init(String body, String contentType, int responseCode) throws OAuthProblemException {
@@ -40,13 +41,4 @@ public class WeixinJSONVerifyAccessTokenResponse extends OAuthAccessTokenRespons
         return new WeixinOAuthTokenVO((String) this.parameters.get(WeiXinOAuthError.ERROR_CODE), (String) this.parameters.get(WeiXinOAuthError.ERROR_MSG));
     }
 
-    @Override
-    public String getOpenid() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public String getNickName() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
 }
