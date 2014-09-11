5e2dfe6b230ee5c3c6b64f929f0a6fd5670770a4
liuling <liuling@sogou-inc.com>
2014-09-11 17:40:18 +0800

    wap2.0之正式注册，注册成功后生成sgid，回转至ru

---
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/account/action/wap/WapV2RegAction.java
@@ -0,0 +1,13 @@
+package com.sogou.upd.passport.web.account.action.wap;
+
+import com.sogou.upd.passport.web.BaseController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liuling
+ * Date: 14-9-11
+ * Time: 下午5:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class WapV2RegAction extends BaseController {
+}
