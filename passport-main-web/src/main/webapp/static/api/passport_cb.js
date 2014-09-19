a62ac046b4d099365640e45be6079f51ec11d8b6
yinyong <yinyong@sogou-inc.com>
2014-09-19 15:12:40 +0800

    wap2.0提交测试

--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -7,7 +7,7 @@
     }else{
         passhref = 'http://s.account.sogoucdn.com/u';
     }
-    scr.src= passhref + '/api/sogou.js?t=2014081948';
+    scr.src= passhref + '/api/sogou.js?t=2014091925';
     if(undefined !==window.__sogoujsStartLoading){
         window.__sogoujsStartLoading = + new Date;
     }
