2f423dd414c7670ebafed7eebca9705787f1b95e
Qiancheng <qiancheng@sogou-inc.com>
2014-11-27 12:11:32 +0800

    leave page

--- passport-main-web/src/main/webapp/static/api/passport.js
+++ passport-main-web/src/main/webapp/static/api/passport.js
@@ -8,5 +8,5 @@
     if (undefined !== window.__sogoujsStartLoading) {
         window.__sogoujsStartLoading = +new Date;
     }
-    document.write('<script type="text/javascript" src="'+ passhref +'/api/sogou.js?t=2014111709"></script>');
+    document.write('<script type="text/javascript" src="'+ passhref +'/api/sogou.js?t=2014112717"></script>');
 })();
