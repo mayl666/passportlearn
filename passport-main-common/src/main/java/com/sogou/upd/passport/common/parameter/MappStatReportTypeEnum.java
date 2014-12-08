12f6a49f96460ed0c0343fc0210b96274de99af8
shipengzhi <shipengzhi@sogou-inc.com>
2014-12-08 17:15:05 +0800

    修复/mapp/stat/report参数type没做校验的bug

---
+++ passport-main-common/src/main/java/com/sogou/upd/passport/common/parameter/MappStatReportTypeEnum.java
@@ -0,0 +1,50 @@
+package com.sogou.upd.passport.common.parameter;
+
+
+import com.google.common.base.Strings;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.sogou.upd.passport.common.utils.PhoneUtil;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 账号类型，第三方，邮箱，手机号码
+ *
+ * @author shipengzhi
+ */
+public enum MappStatReportTypeEnum {
+
+    EXCEPTION("exception"),         //异常
+    INTERFACE("interface"), //接口响应
+    NETFLOW("netFlow"), //流量
+    PRODUCT("product"), //产品
+    COMMLOG("commLog"); //普通日志
+
+    private String type;
+    MappStatReportTypeEnum(String type) {
+        this.type = type;
+    }
+
+    private static final List<String> TYPE_LIST = Lists.newArrayList();
+    static {
+        TYPE_LIST.add(EXCEPTION.toString());
+        TYPE_LIST.add(INTERFACE.toString());
+        TYPE_LIST.add(NETFLOW.toString());
+        TYPE_LIST.add(PRODUCT.toString());
+        TYPE_LIST.add(COMMLOG.toString());
+    }
+
+    public static boolean isSupportType(String type) {
+        return TYPE_LIST.contains(type);
+    }
+
+    @Override
+    public String toString() {
+        return type;
+    }
+
+}
--- passport-main-service/src/main/java/com/sogou/upd/passport/model/MappDeployConfigFactory.java
+++ passport-main-service/src/main/java/com/sogou/upd/passport/model/MappDeployConfigFactory.java
@@ -25,38 +25,39 @@ public class MappDeployConfigFactory {
 
     public static Map getMappConfig() throws IOException {
         if (confMap == null || confMap.isEmpty()) {
-            properties = new Props();
-            InputStream input = MappDeployConfigFactory.class.getClassLoader().getResourceAsStream(RESOURCE_NAME);
-            properties.load(input);
-            //url
-            confMap.putIfAbsent("qqSSOLoginUrl", properties.getValue("qqSSOLoginUrl"));
-            confMap.putIfAbsent("sinaSSOLoginUrl", properties.getValue("sinaSSOLoginUrl"));
-            confMap.putIfAbsent("wxSSOLoginUrl", properties.getValue("wxSSOLoginUrl"));
-            confMap.putIfAbsent("connectSSOLoginUrl", properties.getValue("connectSSOLoginUrl"));
-            confMap.putIfAbsent("connectWapLoginUrl", properties.getValue("connectWapLoginUrl"));
-            confMap.putIfAbsent("logoutUrl", properties.getValue("logoutUrl"));
-            confMap.putIfAbsent("wapLoginUrl", properties.getValue("wapLoginUrl"));
-            confMap.putIfAbsent("getSmsCodeUrl", properties.getValue("getSmsCodeUrl"));
-            confMap.putIfAbsent("registUrl", properties.getValue("registUrl"));
-            confMap.putIfAbsent("findPswUrl", properties.getValue("findPswUrl"));
-            confMap.putIfAbsent("getloginCaptchaUrl", properties.getValue("getloginCaptchaUrl"));
-            confMap.putIfAbsent("statisticsInfoUrl", properties.getValue("statisticsInfoUrl"));
-            confMap.putIfAbsent("statReportUrl", properties.getValue("statReportUrl"));
+            synchronized (MappDeployConfigFactory.class) {
+                properties = new Props();
+                InputStream input = MappDeployConfigFactory.class.getClassLoader().getResourceAsStream(RESOURCE_NAME);
+                properties.load(input);
+                //url
+                confMap.putIfAbsent("qqSSOLoginUrl", properties.getValue("qqSSOLoginUrl"));
+                confMap.putIfAbsent("sinaSSOLoginUrl", properties.getValue("sinaSSOLoginUrl"));
+                confMap.putIfAbsent("wxSSOLoginUrl", properties.getValue("wxSSOLoginUrl"));
+                confMap.putIfAbsent("connectSSOLoginUrl", properties.getValue("connectSSOLoginUrl"));
+                confMap.putIfAbsent("connectWapLoginUrl", properties.getValue("connectWapLoginUrl"));
+                confMap.putIfAbsent("logoutUrl", properties.getValue("logoutUrl"));
+                confMap.putIfAbsent("wapLoginUrl", properties.getValue("wapLoginUrl"));
+                confMap.putIfAbsent("getSmsCodeUrl", properties.getValue("getSmsCodeUrl"));
+                confMap.putIfAbsent("registUrl", properties.getValue("registUrl"));
+                confMap.putIfAbsent("findPswUrl", properties.getValue("findPswUrl"));
+                confMap.putIfAbsent("getloginCaptchaUrl", properties.getValue("getloginCaptchaUrl"));
+                confMap.putIfAbsent("statisticsInfoUrl", properties.getValue("statisticsInfoUrl"));
+                confMap.putIfAbsent("statReportUrl", properties.getValue("statReportUrl"));
 
-            //threshold
-            confMap.putIfAbsent("statReportSizeMobile", properties.getValue("statReportSizeMobile"));
-            confMap.putIfAbsent("statReportSizeWifi", properties.getValue("statReportSizeWifi"));
-            confMap.putIfAbsent("statReportTimeSpace", properties.getValue("statReportTimeSpace"));
-            confMap.putIfAbsent("httpReadTimeout", properties.getValue("httpReadTimeout"));
-            confMap.putIfAbsent("httpRetryTimes", properties.getValue("httpRetryTimes"));
+                //threshold
+                confMap.putIfAbsent("statReportSizeMobile", properties.getValue("statReportSizeMobile"));
+                confMap.putIfAbsent("statReportSizeWifi", properties.getValue("statReportSizeWifi"));
+                confMap.putIfAbsent("statReportTimeSpace", properties.getValue("statReportTimeSpace"));
+                confMap.putIfAbsent("httpReadTimeout", properties.getValue("httpReadTimeout"));
+                confMap.putIfAbsent("httpRetryTimes", properties.getValue("httpRetryTimes"));
 
-            //switch
-            confMap.putIfAbsent("isExceptionInfo", properties.getValue("isExceptionInfo"));
-            confMap.putIfAbsent("isTrafficStatistics", properties.getValue("isTrafficStatistics"));
-            confMap.putIfAbsent("isInterfaceStatistics", properties.getValue("isInterfaceStatistics"));
-            confMap.putIfAbsent("isProductStatistics", properties.getValue("isProductStatistics"));
-            confMap.putIfAbsent("isDebugLog", properties.getValue("isDebugLog"));
-
+                //switch
+                confMap.putIfAbsent("isExceptionInfo", properties.getValue("isExceptionInfo"));
+                confMap.putIfAbsent("isTrafficStatistics", properties.getValue("isTrafficStatistics"));
+                confMap.putIfAbsent("isInterfaceStatistics", properties.getValue("isInterfaceStatistics"));
+                confMap.putIfAbsent("isProductStatistics", properties.getValue("isProductStatistics"));
+                confMap.putIfAbsent("isDebugLog", properties.getValue("isDebugLog"));
+            }
         }
         return confMap;
     }
