fb5432e15fbeb7776f61dc8f8c922685a27ee164
Qiancheng <qiancheng@sogou-inc.com>
2014-12-10 15:52:31 +0800

    re online

--- passport-main-web/src/main/webapp/static/api/passport_cb.js
+++ passport-main-web/src/main/webapp/static/api/passport_cb.js
@@ -7,7 +7,7 @@
     }else{
         passhref = 'http://s.account.sogoucdn.com/u';
     }
-    scr.src= passhref + '/api/sogou.js?t=2014121018';
+    scr.src= passhref + '/api/sogou.js?t=2014121016';
     if(undefined !==window.__sogoujsStartLoading){
         window.__sogoujsStartLoading = + new Date;
     }
