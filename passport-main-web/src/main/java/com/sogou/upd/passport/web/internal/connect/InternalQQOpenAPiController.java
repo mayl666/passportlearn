1d7ba0f056c4f95f3d3fcb51995b137f223ee5e4
laozhiyi <laozhiyi>
2015-01-09 20:54:46 +0800

    test

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/internal/connect/InternalQQOpenAPiController.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/internal/connect/InternalQQOpenAPiController.java
@@ -148,7 +148,7 @@ public class InternalQQOpenAPiController extends BaseController {
             result.setSuccess(true);
             return result.toString();
         } catch (Exception e) {
-            logger.error("get qq friends error. ", e.getStackTrace());
+            logger.error("get qq friends error. ", e);
             result.setCode(ErrorUtil.SYSTEM_UNKNOWN_EXCEPTION);
             return result.toString();
         } finally {
