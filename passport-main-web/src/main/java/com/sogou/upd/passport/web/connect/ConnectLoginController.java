664472653d136dd72d92e2464311af963c76ff8f
shipengzhi <shipengzhi@sogou-inc.com>
2015-04-28 15:33:54 +0800

    微博wap登录，增加对2020的临时限制

--- passport-main-web/src/main/java/com/sogou/upd/passport/web/connect/ConnectLoginController.java
+++ passport-main-web/src/main/java/com/sogou/upd/passport/web/connect/ConnectLoginController.java
@@ -4,6 +4,7 @@ import com.google.common.base.Strings;
 import com.sogou.upd.passport.common.CommonConstant;
 import com.sogou.upd.passport.common.lang.StringUtil;
 import com.sogou.upd.passport.common.model.useroperationlog.UserOperationLog;
+import com.sogou.upd.passport.common.parameter.AccountDomainEnum;
 import com.sogou.upd.passport.common.parameter.AccountTypeEnum;
 import com.sogou.upd.passport.common.utils.ErrorUtil;
 import com.sogou.upd.passport.common.utils.ServletUtil;
@@ -66,7 +67,7 @@ public class ConnectLoginController extends BaseConnectController {
                 return "empty";
             }
             // 如果是糖猫的wap登录，使用通行证默认appid
-            if("2020".equals(connectLoginParams.getClient_id()) && !Strings.isNullOrEmpty(connectLoginParams.getThird_appid())){
+            if("2020".equals(connectLoginParams.getClient_id()) && "sina".equals(providerStr) && !Strings.isNullOrEmpty(connectLoginParams.getThird_appid())){
                   connectLoginParams.setThird_appid("1279688155");
             }
 
