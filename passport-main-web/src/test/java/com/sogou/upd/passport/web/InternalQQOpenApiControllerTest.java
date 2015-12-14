f7efb5c508e6a4f18802cc209889581ebd9dcb37
nahongxu <nahongxu@sogou-inc.com>
2015-12-14 19:05:20 +0800

    增加单元测试

--- passport-main-web/src/test/java/com/sogou/upd/passport/web/InternalQQOpenApiControllerTest.java
+++ passport-main-web/src/test/java/com/sogou/upd/passport/web/InternalQQOpenApiControllerTest.java
@@ -1,12 +1,21 @@
 package com.sogou.upd.passport.web;
 
+import com.sogou.upd.passport.common.apache_asynhttpclient.ApacheAsynHttpClient;
+import com.sogou.upd.passport.common.math.AES;
 import com.sogou.upd.passport.common.model.httpclient.RequestModel;
 import com.sogou.upd.passport.common.parameter.HttpMethodEnum;
+import com.sogou.upd.passport.common.result.Result;
+import com.sogou.upd.passport.common.utils.JacksonJsonMapperUtil;
 import com.sogou.upd.passport.common.utils.SGHttpClient;
+import com.sogou.upd.passport.manager.api.connect.ConnectApiManager;
+import com.sogou.upd.passport.manager.connect.impl.QQOpenAPIManagerImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,6 +27,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:spring-config-test.xml")
 public class InternalQQOpenApiControllerTest {
+    @Autowired
+    private ConnectApiManager sgConnectApiManager;
 
     @Test
     public void testHttpClient(){
@@ -30,5 +41,31 @@ public class InternalQQOpenApiControllerTest {
         System.out.println("========================================================");
         System.out.println(str);
         System.out.println("========================================================");
+    }
+
+    @Test
+    public void testQcloud() throws Exception{
+        String qCloudUrl="http://115.159.57.127:8888/internal/qq/friends_aesinfo";
+        String userId="E74BEC2F5729AB12495986504FA64826@qq.sohu.com";
+        int clientId=2040;
+        String third_appid=null;
+        Result obtainTKeyResult = sgConnectApiManager.obtainTKey(userId, clientId, third_appid);
+        if (!obtainTKeyResult.isSuccess()) {
+            System.out.println(obtainTKeyResult.toString());
+            return;
+        }
+
+        String tKey = (String) obtainTKeyResult.getModels().get("tKey");
+
+        RequestModel requestModel = new RequestModel(qCloudUrl);
+        requestModel.addParam("userid", userId);
+        requestModel.addParam("tKey", tKey);
+        requestModel.setHttpMethodEnum(HttpMethodEnum.POST);
+//            String returnVal = SGHttpClient.executeStr(requestModel);
+        String returnVal = ApacheAsynHttpClient.executeStr(requestModel);
+//            asyncHttpClientService.sendPreparePost(GET_QQ_FRIENDS_AES_URL);
+        String str = AES.decryptURLSafeString(returnVal, QQOpenAPIManagerImpl.TKEY_SECURE_KEY);
+       System.out.println("result:"+str);
+
     }
 }
