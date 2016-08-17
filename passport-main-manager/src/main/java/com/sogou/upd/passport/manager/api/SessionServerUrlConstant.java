c48513df2fa9fee0232c570cbd919016402fc72c
wanghuaqing <wanghuaqing@sogou-inc.com>
2016-08-17 17:48:27 +0800

    更换项目中私网域名

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/api/SessionServerUrlConstant.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/api/SessionServerUrlConstant.java
@@ -1,12 +1,14 @@
 package com.sogou.upd.passport.manager.api;
 
+import org.springframework.beans.factory.annotation.Value;
+
 /**
  * User: mayan
  * Date: 13-12-4
  * Time: 下午9:11
  */
 public class SessionServerUrlConstant {
-    private static final String BASE_URL = "http://session.account.sogou.com.z.sogou-op.org/";
+    private static final String BASE_URL = "http://session.account.sogou/";
     public static final String CREATE_SESSION = BASE_URL + "set_session";
     public static final String REMOVE_SESSION = BASE_URL + "del_session";
     public static final String VERIFY_SID = BASE_URL + "verify_sid";
