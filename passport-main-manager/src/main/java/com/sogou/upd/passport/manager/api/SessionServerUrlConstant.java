747ea489c0f29c119b7f980ee0956ece1ddc15fa
wanghuaqing <wanghuaqing@sogou-inc.com>
2016-08-19 16:14:43 +0800

    暂将 session server 的私网域名改成旧域名，待新域名配置添加好后再改回

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/api/SessionServerUrlConstant.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/api/SessionServerUrlConstant.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
  * Time: 下午9:11
  */
 public class SessionServerUrlConstant {
-    private static final String BASE_URL = "http://session.account.sogou/";
+    private static final String BASE_URL = "http://session.account.sogou.com.z.sogou-op.org/";
     public static final String CREATE_SESSION = BASE_URL + "set_session";
     public static final String REMOVE_SESSION = BASE_URL + "del_session";
     public static final String VERIFY_SID = BASE_URL + "verify_sid";
