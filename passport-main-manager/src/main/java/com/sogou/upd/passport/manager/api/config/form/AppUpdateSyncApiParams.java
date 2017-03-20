4b900f6a183af3b4ef09a3080a3556d9fcf1f84c
wanghuaqing <wanghuaqing@sogou-inc.com>
2017-03-20 19:38:25 +0800

    添加第三方 connect_config 同步接口；优化 app 同步接口；删除 app_config 同时删除相应 connect_config 信息

--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/api/config/form/AppUpdateSyncApiParams.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/api/config/form/AppUpdateSyncApiParams.java
@@ -4,55 +4,50 @@ import com.google.common.base.Objects;
 
 import com.sogou.upd.passport.manager.api.BaseApiParams;
 
-import org.hibernate.validator.constraints.NotBlank;
-
 import javax.validation.constraints.Min;
 
 /**
  * 同步应用参数
  */
-public class AppSyncApiParams extends BaseApiParams {
+public class AppUpdateSyncApiParams extends BaseApiParams {
     @Min(value = 10000, message = "应用 id 错误")
     private int appId;
-    @NotBlank(message = "应用名称不允许为空")
     private String appName;
-    @NotBlank(message = "服务端密钥不允许为空")
     private String serverSecret;
-    @NotBlank(message = "客户端密钥不允许为空")
     private String clientSecret;
-    
+
     public int getAppId() {
         return appId;
     }
-    
+
     public void setAppId(int appId) {
         this.appId = appId;
     }
-    
+
     public String getAppName() {
         return appName;
     }
-    
+
     public void setAppName(String appName) {
         this.appName = appName;
     }
-    
+
     public String getServerSecret() {
         return serverSecret;
     }
-    
+
     public void setServerSecret(String serverSecret) {
         this.serverSecret = serverSecret;
     }
-    
+
     public String getClientSecret() {
         return clientSecret;
     }
-    
+
     public void setClientSecret(String clientSecret) {
         this.clientSecret = clientSecret;
     }
-    
+
     @Override
     public String toString() {
         return Objects.toStringHelper(this)
--- passport-main-manager/src/main/java/com/sogou/upd/passport/manager/sync/SyncManager.java
+++ passport-main-manager/src/main/java/com/sogou/upd/passport/manager/sync/SyncManager.java
@@ -1,7 +1,10 @@
 package com.sogou.upd.passport.manager.sync;
 
 import com.sogou.upd.passport.common.result.Result;
-import com.sogou.upd.passport.manager.api.config.form.AppSyncApiParams;
+import com.sogou.upd.passport.manager.api.config.form.AppAddSyncApiParams;
+import com.sogou.upd.passport.manager.api.config.form.AppUpdateSyncApiParams;
+import com.sogou.upd.passport.manager.api.config.form.ThirdAddSyncApiParams;
+import com.sogou.upd.passport.manager.api.config.form.ThirdUpdateSyncApiParams;
 
 /**
  * 同步 manager
@@ -10,21 +13,35 @@ public interface SyncManager {
     /**
      * 新增应用配置
      *
-     * @param appSyncApiParams 需要新增的应用
+     * @param appAddSyncApiParams 需要新增的应用
      */
-    public Result addApp(AppSyncApiParams appSyncApiParams);
-    
+    public Result addApp(AppAddSyncApiParams appAddSyncApiParams);
+
     /**
      * 修改应用配置
      *
-     * @param appSyncApiParams 需要修改的应用
+     * @param appUpdateSyncApiParams 需要修改的应用
      */
-    public Result updateApp(AppSyncApiParams appSyncApiParams);
-    
+    public Result updateApp(AppUpdateSyncApiParams appUpdateSyncApiParams);
+
     /**
      * 删除应用配置
      *
      * @param clientId 需要删除的应用
      */
     public Result deleteApp(int clientId);
+
+    /**
+     * 新增第三方配置
+     *
+     * @param thirdAddSyncApiParams 需要新增的第三方信息
+     */
+    public Result addThird(ThirdAddSyncApiParams thirdAddSyncApiParams);
+
+    /**
+     * 删除第三方配置
+     *
+     * @param thirdUpdateSyncApiParams 需要删除的第三方信息
+     */
+    public Result deleteThird(ThirdUpdateSyncApiParams thirdUpdateSyncApiParams);
 }
